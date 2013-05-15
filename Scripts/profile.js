6990b8fd99edb538d4fd04bb6651e9021e415c2c
Steam Database <freeman@steamdb.info>
2013-05-15 17:00:17 +0000

    A  Scripts/profile.js, M  urls.txt

---
+++ Scripts/profile.js
@@ -0,0 +1,262 @@
+
+
+/* returns a jquery deferred object, .done() means an invite was sent (or attempted), .fail() indicates they dismissed the modal */
+function PresentGroupInviteOptions( rgFriendsToInvite )
+{
+	// this deferred will succeed if an invite is succesfully sent, fail if the user dismisses the modal or the invite AJAX fails
+	var deferred = new jQuery.Deferred();
+
+	var Modal = ShowDialog( 'Invite to join your group', '<div class="group_invite_throbber"><img src="http://cdn.steamcommunity.com/public/images/login/throbber.gif"></div>' );
+	var $ListElement = $J('<div/>', {'class': 'newmodal_content_innerbg'} );
+
+	var bBulkFriendInvite = false;
+	var steamIDInvitee = g_rgProfileData['steamid'];
+	var strProfileURL = g_rgProfileData['url'];
+
+	// see if this is a request to bulk invite a group of friends
+	if ( rgFriendsToInvite && rgFriendsToInvite instanceof Array )
+	{
+		if ( rgFriendsToInvite.length == 1 )
+		{
+			steamIDInvitee = rgFriendsToInvite[0];
+			strProfileURL = 'http://steamcommunity.com/profiles/' + steamIDInvitee + '/';
+		}
+		else
+		{
+			// true bulk invite
+			steamIDInvitee = rgFriendsToInvite;
+			bBulkFriendInvite = true;
+		}
+	}
+
+	// if the modal is dismissed , we'll cancel the deferred object.  We capture this in a closure so that we can dismiss the modal without affecting
+	//	the deferred object if the user actually picks something (in which case the deferred object will be the success of the AJAX invite action)
+	var fnOnModalDismiss = function() { deferred.reject() };
+
+	$J.get( strProfileURL + 'ajaxgroupinvite?new_profile=1' + ( bBulkFriendInvite ? '&bulk=1' : '' ), function( html ) {
+		Modal.GetContent().find( '.newmodal_content').html('');	// erase the throbber
+		Modal.GetContent().find( '.newmodal_content').append( $ListElement );
+		$ListElement.html( html );
+		Modal.AdjustSizing();
+		$ListElement.children( '.group_list_results' ).children().each( function () {
+			var groupid = this.getAttribute( 'data-groupid' );
+			if ( groupid )
+			{
+				$J(this).click( function() {
+					fnOnModalDismiss = function() {;};	// don't resolve the deferred on modal dismiss anymore, user has picked something
+					InviteUserToGroup( Modal, groupid, steamIDInvitee)
+					.done( function() { deferred.resolve(); } )
+					.fail( function() { deferred.reject(); } );
+				} );
+			}
+		});
+	});
+
+	Modal.done( function() {fnOnModalDismiss();} );
+
+	return deferred.promise();
+}
+
+function InviteUserToGroup( Modal, groupID, steamIDInvitee )
+{
+	var params = {
+		json: 1,
+		type: 'groupInvite',
+		group: groupID,
+		sessionID: g_sessionID
+	};
+
+	if ( steamIDInvitee instanceof Array )
+		params.invitee_list = V_ToJSON( steamIDInvitee );
+	else
+		params.invitee = steamIDInvitee;
+
+	return $J.ajax( { url: 'http://steamcommunity.com/actions/GroupInvite',
+		data: params,
+		type: 'POST'
+	} ).done( function( data ) {
+		Modal && Modal.Dismiss();
+		var strMessage = 'Invitation Sent!';
+		if ( data.duplicate )
+		{
+			strMessage += '<br>Some invites were not sent because the recipients are already in the group or have already received invites.';
+		}
+		ShowAlertDialog( 'Invite to join your group', strMessage );
+
+	}).fail( function( data ) {
+		Modal && Modal.Dismiss();
+		ShowAlertDialog( '', data.results ? data.results : 'Error processing your request. Please try again.' );
+	});
+}
+
+function RemoveFriend()
+{
+	var steamid = g_rgProfileData['steamid'];
+	var strPersonaName = g_rgProfileData['personaname'];
+
+	ShowConfirmDialog( 'Remove friend',
+		'Are you sure you want to remove %s from your friend list?'.replace( /%s/, strPersonaName ),
+		'Remove friend'
+	).done( function() {
+		$J.post(
+			'http://steamcommunity.com/actions/RemoveFriendAjax',
+			{sessionID: g_sessionID, steamid: steamid }
+		).done( function() {
+			ShowAlertDialog( 'Remove friend',
+				'%s has been removed from your friends list.'.replace( /%s/, strPersonaName )
+			).done( function() {
+				// reload the page when they click OK, so we update friend state
+				window.location.reload();
+			} );
+		} ).fail( function() {
+			ShowAlertDialog( 'Remove friend',
+				'Error processing your request. Please try again.'
+			);
+		} );
+	} );
+}
+
+function AddFriend( bRespondingToInvite )
+{
+	var steamid = g_rgProfileData['steamid'];
+	var strPersonaName = g_rgProfileData['personaname'];
+
+	$J.post(
+		'http://steamcommunity.com/actions/AddFriendAjax',
+		{sessionID: g_sessionID, steamid: steamid }
+	).done( function() {
+		if ( !bRespondingToInvite )
+		{
+			ShowAlertDialog( 'Add Friend',
+				'Friend invite sent.'
+			);
+		}
+		else
+		{
+			ShowAlertDialog( 'Accept Friend Request',
+				'Friend request accepted'
+			).done( function() { window.location.reload(); } );
+		}
+	} ).fail( function() {
+		ShowAlertDialog( 'Add Friend',
+			'Error adding friend. Please try again.'
+		);
+	} );
+}
+
+function ConfirmBlock()
+{
+	var steamid = g_rgProfileData['steamid'];
+	var strPersonaName = g_rgProfileData['personaname'];
+
+	ShowConfirmDialog( 'Block all communication',
+		'You are about to block all communication with %s.'.replace( /%s/, strPersonaName ),
+		'Yes, block them'
+	).done( function() {
+			$J.post(
+				'http://steamcommunity.com/actions/BlockUserAjax',
+				{sessionID: g_sessionID, steamid: steamid }
+			).done( function() {
+				ShowAlertDialog( 'Block all communication',
+					'You have blocked all communications with this player.'
+				);
+			} ).fail( function() {
+				ShowAlertDialog( 'Block all communication',
+					'Error processing your request. Please try again.'
+				);
+			} );
+		} );
+}
+
+function ToggleManageFriends()
+{
+	if ( $J('#manage_friends_actions_ctn').is( ':hidden' ) )
+	{
+		$J('#manage_friends_btn').find( '.btn_details_arrow').removeClass( 'down').addClass( 'up' );
+		$J('#manage_friends_actions_ctn').slideDown( 'fast' );
+		$J('div.manage_friend_checkbox').show();
+	}
+	else
+	{
+		$J('#manage_friends_btn').find( '.btn_details_arrow').removeClass( 'up').addClass( 'down' );
+		$J('#manage_friends_actions_ctn').slideUp( 'fast' );
+		$J('div.manage_friend_checkbox').hide();
+	}
+}
+
+function ManageFriendsInviteToGroup( $Form, groupid )
+{
+	$Form.find('input[type="checkbox"]')
+	var rgFriendSteamIDs = [];
+	$Form.find( 'input[type=checkbox]' ).each( function() {
+		if ( this.checked )
+			rgFriendSteamIDs.push( $J(this).attr( 'data-steamid' ) );
+	} );
+	if ( rgFriendSteamIDs.length > 0 )
+	{
+		if ( groupid )
+		{
+			// specific group
+			InviteUserToGroup( null /* no modal window */, groupid, rgFriendSteamIDs ).done( function() {
+				$Form.find('input[type=checkbox]').prop( 'checked', false );
+			});
+		}
+		else
+		{
+			// ask the user which group to invite to
+			PresentGroupInviteOptions( rgFriendSteamIDs).done( function() {
+				$Form.find('input[type=checkbox]').prop( 'checked', false );
+			});
+		}
+	}
+	else
+	{
+		ShowAlertDialog( 'Invite to join your group', 'You have not selected any friends.' );
+	}
+}
+
+function ManageFriendsExecuteBulkAction( $Form, strActionName )
+{
+	if ( $Form.find('input[type=checkbox]:checked').length == 0 )
+	{
+		ShowAlertDialog( '', 'You have not selected any friends.' );
+		return;
+	}
+
+	$Form.find('input[name=action]').val( strActionName );
+	$Form.submit();
+}
+
+function ManageFriendsConfirmBulkAction( $Form, strActionName, strTitle, strSingluarDescription, strPluralDescription )
+{
+	var cFriendsSelected = $Form.find('input[type=checkbox]:checked').length;
+	if ( cFriendsSelected == 0 )
+	{
+		ShowAlertDialog( strTitle, 'You have not selected any friends.' );
+		return;
+	}
+
+	var strDescription = strSingluarDescription;
+	if ( cFriendsSelected != 1 )
+		strDescription = strPluralDescription.replace( /%s/, cFriendsSelected );
+
+	ShowConfirmDialog( strTitle, strDescription).done( function() {
+		ManageFriendsExecuteBulkAction( $Form, strActionName );
+	});
+}
+
+function ManageFriendsBlock( $Form )
+{
+	ManageFriendsConfirmBulkAction( $Form, 'ignore', 'Block',
+		'Are you sure you want to block this friend?' + ' ' + 'You will no longer be able to send or receive messages or invites with this player.',
+		'Are you sure you want to block these %s friends?' + ' ' + 'You will no longer be able to send or receive messages or invites with these players.');
+}
+
+function ManageFriendsRemove( $Form )
+{
+	ManageFriendsConfirmBulkAction( $Form, 'remove', 'Remove Friend',
+		'Are you sure you want to remove this friend?' + ' ' + 'This player will no longer appear in your friends list and you will not be able to communicate with them.',
+		'Are you sure you want to remove these %s friends?' + ' ' + 'These players will no longer appear in your friends list and you will not be able to communicate with them.');
+}
+
+
