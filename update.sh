b88300ac4b0934274e2eb9908baa85d492874e6f
Steam Database <freeman@steamdb.info>
2013-05-17 23:05:13 +0000

    Remove leading space from commit message

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 
 php update.php
 
-git commit -a -m "`git status --porcelain | sed '{:q;N;s/\n/, /g;t q}'`"
+git commit -a -m "`git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g'`"
 git push
