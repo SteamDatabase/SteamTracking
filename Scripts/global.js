58c43bcb164ce93cb5c1187a7013d89ee35d0181
Steam Database <freeman@steamdb.info>
2013-05-18 01:00:22 +0000

    M ClientManifest/steam_client_publicbeta_win32.manifest,  M ItemSchema/API/Dota2.vdf,  M ItemSchema/Dota2.json,  M Scripts/global.js,  M Styles/profilev2.css

--- Scripts/global.js
+++ Scripts/global.js
@@ -2413,6 +2413,15 @@ CGameSelectorOwnedGames = Class.create( CGameSelector, {
 					}
 				}
 
+				rgMatchingGames.sort( function( a, b ) {
+					if ( a.name < b.name )
+						return -1;
+					else if ( b.name < a.name )
+						return 1;
+					else
+						return 0;
+				} );
+
 				this.UpdateListWithOptions( rgMatchingGames );
 			}
 		}
