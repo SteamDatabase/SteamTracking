9c34151a1cc4421f994569238fdefee294cc99a1
Steam Database <freeman@steamdb.info>
2013-06-09 13:35:06 +0000

    Lock update to single process, so it doesn't conflict with manual runs

--- update.sh
+++ update.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 
+LOCKFILE=update.lock
+
+if [ -f "$LOCKFILE" ] && kill -0 `cat $LOCKFILE` 2>/dev/null; then
+	echo Still running
+	exit 1
+fi
+
+echo $$ > $LOCKFILE
+
 php update.php $1
 
 git commit -a -m "`git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g'`"
 git push
+
+rm $LOCKFILE
