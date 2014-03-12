91c87476b4f60627a28dc42a6e95718a5bfeb443
Steam Database <freeman@steamdb.info>
2014-03-12 23:12:26 +0000

    M  Scripts/checkout.js, M  Scripts/global.js, M  Scripts/refundpurchase.js, M  Scripts/store_main.js, M  Styles/buttons.css, M  Styles/economy_market.css, M  Styles/globalv2.css, M  Styles/groupv2.css, M  Styles/profile_friendactivity.css, M  Styles/styles_cartv5.css, M  Styles/styles_gamev5.css, M  Styles/styles_storev5.css, M  Styles/workshop_itemdetails.css

--- Scripts/refundpurchase.js
+++ Scripts/refundpurchase.js
@@ -156,7 +156,7 @@ function ConfirmRefund(transid)
 		      			OnConfirmRefundFailure( transid, 2 );
 		      		}
 		      	   	// Success...
-		      	   	if ( result.success == 1 )
+		      	   	if ( result.success == 1 || result.success == 22 )
 		      	   	{
 		      	   		OnConfirmRefundSuccess( transid, result );
 		      	   		return;
@@ -215,3 +215,89 @@ function OnConfirmRefundFailure( transid, result )
 	}
 }
 
+
+var g_bCancelPendingRefundRunning = false;
+function CancelPendingRefund(refundid)
+{
+		
+		if( g_bCancelPendingRefundRunning )
+		return;
+
+	try 
+	{
+				g_bCancelPendingRefundRunning = true;
+
+		new Ajax.Request('https://store.steampowered.com/account/cancelpendingrefund/',
+		{
+		    method:'post',
+		    parameters: { 
+				'refundid' : refundid
+			},
+		    onSuccess: function(transport){
+		    	g_bCancelPendingRefundRunning = false;
+				if ( transport.responseText ){
+					try {
+						var result = transport.responseText.evalJSON(true);
+		      		} catch ( e ) {
+		      			// Failure
+		      			OnCancelPendingRefundFailure( refundid, 2 );
+		      		}
+		      	   	// Success...
+		      	   	if ( result.success == 1 )
+		      	   	{
+		      	   		OnCancelPendingRefundSuccess( refundid, result );
+		      	   		return;
+		      	   	}
+		      	   	else
+		      	   	{
+		      	   		OnCancelPendingRefundFailure( refundid, result.success );
+		      	   		return;
+		      	   	}
+			  	}
+			  	
+								OnCancelPendingRefundFailure( refundid, 2 );
+		    },
+		    onFailure: function(){
+								g_bCancelPendingRefundRunning = false;
+				OnCancelPendingRefundFailure( refundid, 2 );
+			}
+		});
+	} 
+	catch(e) 
+	{
+		ReportYourAccountJSError( 'Failed making AJAX call to CancelPendingRefund', e );
+	}
+}
+
+
+function OnCancelPendingRefundSuccess( refundid, results )
+{
+	try 
+	{
+		$('refund_error').style.display = 'none';		
+		$('refundPage1').style.display = 'none';
+		$('refundPage2').style.display = 'none';
+		$('refundPage3').style.display = 'none';
+		$('refundPage4').style.display = 'block';
+		$('refundConfirmation').innerHTML = 'Your refund request has been cancelled.';
+	} 
+	catch( e ) 
+	{
+		ReportYourAccountJSError( 'Failed in OnCancelPendingRefundSuccess()', e );
+	}
+}
+
+function OnCancelPendingRefundFailure( refundid, result )
+{
+	try 
+	{
+		$('refund_error').innerHTML = 'There was a problem processing your refund at this time.<br/>Please contact <a href="%s">Steam Support</a>';
+		$('refund_error').style.display = 'block';
+		Effect.ScrollTo( 'refund_error' );
+		new Effect.Highlight( 'refund_error', { endcolor : '#000000', startcolor : '#ff9900' } );		
+	} 
+	catch (e) 
+	{
+		ReportYourAccountJSError( 'Failed in OnCancelPendingRefundFailure()', e );
+	}
+}
