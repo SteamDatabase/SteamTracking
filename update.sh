36af2652eaa7011611545247d2b6977d04e4e53e
Steam Database <freeman@steamdb.info>
2014-05-29 14:30:20 +0000

    M  update.sh

--- update.sh
+++ update.sh
@@ -2,17 +2,17 @@
 
 LOCKFILE=update.lock
 
-if [ -f "$LOCKFILE" ] && kill -0 `cat $LOCKFILE` 2>/dev/null; then
+if [ -f "$LOCKFILE" ] && kill -0 "$(cat $LOCKFILE)" 2>/dev/null; then
 	echo Still running
 	exit 1
 fi
 
 echo $$ > $LOCKFILE
 
-php update.php $1
+php update.php "$1"
 
 git add -A
-git commit -a -m "`git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g'`"
+git commit -a -m "$(git status --porcelain | sed '{:q;N;s/\n/, /g;t q}' | sed 's/^ *//g')"
 git push
 
 rm $LOCKFILE
