b21d02917d4897109d1c9d2cfa66b8d46f89e61b
Steam Database <freeman@steamdb.info>
2014-06-26 01:45:26 +0000

    M  Scripts/market.js

--- Scripts/market.js
+++ Scripts/market.js
@@ -1913,7 +1913,14 @@ ItemActivityTicker = {
 			var line_item = this.m_rgActivityShown.shift();
 
 			// animate thie item up
-			$J(line_item).animate( {'margin-top':'-40px','opacity':'0'}, 300, 'swing', function() { $J(line_item).remove(); } );
+            try
+            {
+    			$J(line_item).animate( {'margin-top':'-40px','opacity':'0'}, 300, 'swing', function() { $J(line_item).remove(); } );
+            }
+            catch( err )
+            {
+                // TODO: Update jQuery
+            }
 		}
 
 		// add back items
