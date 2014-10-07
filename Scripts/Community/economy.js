989c844b6f9c90557ffbe4137bb6307a474d6af5
Steam Database <freeman@steamdb.info>
2014-10-07 23:15:19 +0000

    :person_frowning: M  Scripts/Community/economy.js, M  Scripts/Community/market.js, M  Scripts/Community/profile_gameslist_functions.js, M  Scripts/shared_global.js, M  Styles/Community/apphub_guides.css, M  Styles/Community/apphub_home.css, M  Styles/Community/discussions.css, M  Styles/Community/forums.css, M  Styles/Community/global.css, M  Styles/Community/greenlight.css, M  Styles/Community/group_list.css, M  Styles/Community/groupadmin.css, M  Styles/Community/groupv2.css, M  Styles/Community/profile.css, M  Styles/Community/profile_commentnotifications.css, M  Styles/Community/profile_friendactivity.css, M  Styles/Community/profile_games_list.css, M  Styles/Community/profile_sharedfiles.css, M  Styles/Community/workshop.css, M  Styles/Community/workshop_itemdetails.css, M  Styles/Community/workshop_userfiles.css, M  Styles/apphub.css, M  Styles/buttons.css, M  Styles/globalv2.css, M  Styles/shared_global.css, M  Styles/user_reviews.css

--- Scripts/Community/economy.js
+++ Scripts/Community/economy.js
@@ -2858,30 +2858,12 @@ SellItemDialog = {
 		hideModal( 'market_sell_dialog' );
 	},
 
-	GetPriceValueAsInt: function( strAmount ) {
-		var nAmount;
-		if ( !strAmount )
-		{
-			return 0;
-		}
-
-		// strip the currency symbol, set commas to periods, set .-- to .00
-		strAmount = strAmount.replace( GetCurrencySymbol( GetCurrencyCode( g_rgWalletInfo['wallet_currency'] ) ), '' ).replace( ',', '.' ).replace( '.--', '.00');
-
-		var flAmount = parseFloat( strAmount ) * 100;
-		nAmount = Math.round( isNaN(flAmount) ? 0 : flAmount );
-
-		nAmount = Math.max( nAmount, 0 );
-		return nAmount;
-	},
-
-
 	GetPriceAsInt: function() {
-		return this.GetPriceValueAsInt( $('market_sell_currency_input').value );
+		return GetPriceValueAsInt( $('market_sell_currency_input').value );
 	},
 
 	GetBuyerPriceAsInt: function() {
-		return this.GetPriceValueAsInt( $('market_sell_buyercurrency_input').value );
+		return GetPriceValueAsInt( $('market_sell_buyercurrency_input').value );
 	},
 
 	GetQuantityAsInt: function() {
@@ -3732,117 +3714,6 @@ function InventoryDismissNewItems( elNewItems )
 {
 	new Effect.BlindUp( elNewItems, {duration: 0.5 } );
 	document.cookie='tsNewItemsPreviousLastChecked=0';
-}
-
-function CurrencyIsWalletFunds( currency )
-{
-	return currency.appid == 753 && currency.contextid == 4;
-}
-
-function ConvertToTheirCurrency( amount )
-{
-	var flAmount = amount * g_rgWalletInfo['wallet_conversion_rate'];
-		var nAmount = Math.floor( isNaN(flAmount) ? 0 : flAmount );
-
-	return Math.max( nAmount, 0 );
-}
-
-function ConvertToOurCurrency( amount )
-{
-		var flAmount = g_rgWalletInfo['wallet_inverse_conversion_rate'] * ( amount  );
-
-	var nAmount = Math.ceil( isNaN(flAmount) ? 0 : flAmount );
-	nAmount = Math.max( nAmount, 0 );
-
-	// verify the amount. we may be off by a cent.
-	if ( ConvertToTheirCurrency( nAmount ) != amount )
-	{
-		var i;
-		for ( i = nAmount - 2; i <= nAmount + 2; i++ )
-		{
-			if ( ConvertToTheirCurrency( i ) == amount )
-			{
-				nAmount = i;
-				break;
-			}
-		}
-	}
-
-	return nAmount;
-}
-
-function ConvertToOurCurrencyForDisplay( amount )
-{
-	var flAmount = amount * g_rgWalletInfo['wallet_inverse_conversion_rate'];
-		var nAmount = Math.floor( isNaN(flAmount) ? 0 : flAmount );
-
-	return Math.max( nAmount, 0 );
-}
-
-function CalculateFeeAmount( amount, publisherFee )
-{
-	if ( !g_rgWalletInfo['wallet_fee'] )
-		return 0;
-
-	publisherFee = ( typeof publisherFee == 'undefined' ) ? 0 : publisherFee;
-
-	// Since CalculateFeeAmount has a Math.floor, we could be off a cent or two. Let's check:
-	var iterations = 0; // shouldn't be needed, but included to be sure nothing unforseen causes us to get stuck
-	var nEstimatedAmountOfWalletFundsReceivedByOtherParty = parseInt( ( amount - parseInt( g_rgWalletInfo['wallet_fee_base'] ) ) / ( parseFloat( g_rgWalletInfo['wallet_fee_percent'] ) + parseFloat( publisherFee ) + 1 ) );
-
-	var bEverUndershot = false;
-	var fees = CalculateAmountToSendForDesiredReceivedAmount( nEstimatedAmountOfWalletFundsReceivedByOtherParty, publisherFee );
-	while ( fees.amount != amount && iterations < 10 )
-	{
-		if ( fees.amount > amount )
-		{
-			if ( bEverUndershot )
-			{
-				fees = CalculateAmountToSendForDesiredReceivedAmount( nEstimatedAmountOfWalletFundsReceivedByOtherParty - 1, publisherFee );
-				fees.steam_fee += ( amount - fees.amount );
-				fees.fees += ( amount - fees.amount );
-				fees.amount = amount;
-				break;
-			}
-			else
-			{
-				nEstimatedAmountOfWalletFundsReceivedByOtherParty--;
-			}
-		}
-		else
-		{
-			bEverUndershot = true;
-			nEstimatedAmountOfWalletFundsReceivedByOtherParty++;
-		}
-
-		fees = CalculateAmountToSendForDesiredReceivedAmount( nEstimatedAmountOfWalletFundsReceivedByOtherParty, publisherFee );
-		iterations++;
-	}
-
-	// fees.amount should equal the passed in amount
-
-	return fees;
-}
-
-function CalculateAmountToSendForDesiredReceivedAmount( receivedAmount, publisherFee )
-{
-	if ( !g_rgWalletInfo['wallet_fee'] )
-	{
-		return receivedAmount;
-	}
-
-	publisherFee = ( typeof publisherFee == 'undefined' ) ? 0 : publisherFee;
-
-	var nSteamFee = parseInt( Math.floor( Math.max( receivedAmount * parseFloat( g_rgWalletInfo['wallet_fee_percent'] ), g_rgWalletInfo['wallet_fee_minimum'] ) + parseInt( g_rgWalletInfo['wallet_fee_base'] ) ) );
-	var nPublisherFee = parseInt( Math.floor( publisherFee > 0 ? Math.max( receivedAmount * publisherFee, 1 ) : 0 ) );
-	var nAmountToSend = receivedAmount + nSteamFee + nPublisherFee;
-
-	return {
-		steam_fee: nSteamFee,
-		publisher_fee: nPublisherFee,
-		fees: nSteamFee + nPublisherFee,
-		amount: parseInt( nAmountToSend )
-	};
 }
 
 function SelectItemDialogOnSelect()
