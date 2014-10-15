7ebe73e414e95aa7f5d71b5a2a76a9087e4442ab
Steam Database <freeman@steamdb.info>
2014-10-15 18:00:45 +0000

    :boy: M  Scripts/Store/youraccount.js

--- Scripts/Store/youraccount.js
+++ Scripts/Store/youraccount.js
@@ -119,6 +119,7 @@ function OnUpdateSubscriptionSuccess( agreementid, cancelTerms, bBillingAgreemen
 			$('subtext_subscription_'+agreementid).innerHTML = results.terms;
 			$('link_subscription_'+agreementid).style.visibility = 'visible';
 			$('update_subscription_'+agreementid).style.display = 'none';
+			$('transaction_due_date_'+agreementid).innerHTML = results.next_bill_date;
 		}
 		new Effect.Highlight( 'update_subscription_'+agreementid, { endcolor : '#000000', startcolor : '#ff9900' } );	
 	} 
@@ -141,113 +142,6 @@ function OnUpdateSubscriptionFailure( agreementid, cancel )
 			$('update_subscription_'+agreementid).innerHTML = 'There was a problem updating this subscription.<br/>Please contact <a href="%s">Steam Support</a>';
 		}
 		new Effect.Highlight( 'update_subscription_'+agreementid, { endcolor : '#000000', startcolor : '#ff9900' } );	
-	} 
-	catch (e) 
-	{
-		ReportYourAccountJSError( 'Failed in agreementid()', e );
-	}
-}
-
-
-function ShowRenewSubscriptionDialog(agreementid)
-{
-	$('link_subscription_'+agreementid).style.visibility = 'hidden';
-	$('renew_subscription_'+agreementid).style.display = 'block';
-	
-	new Effect.Highlight( 'renew_subscription_'+agreementid, { endcolor : '#000000', startcolor : '#ff9900' } );	
-}
-
-
-function AbortRenewSubscription(agreementid)
-{
-	$('link_subscription_'+agreementid).style.visibility = 'visible';
-	$('renew_subscription_'+agreementid).style.display = 'none';
-}
-
-
-var g_bRenewSubscriptionRunning = false;
-function RenewSubscription(agreementid, duedate, terms, total, bBillingAgreement)
-{
-		if( g_bRenewSubscriptionRunning )
-		return;
-
-	try 
-	{
-				g_bRenewSubscriptionRunning = true;
-
-		new Ajax.Request('https://store.steampowered.com/account/renewsubscription/',
-		{
-		    method:'post',
-		    parameters: { 
-				'agreementid' : agreementid
-			},
-		    onSuccess: function(transport){
-		    	g_bRenewSubscriptionRunning = false;
-				if ( transport.responseText ){
-					try {
-						var result = transport.responseText.evalJSON(true);
-		      		} catch ( e ) {
-		      			// Failure
-		      			OnRenewSubscriptionFailure( agreementid );
-		      		}
-		      	   	// Success...
-		      	   	if ( result.success == 1 )
-		      	   	{
-		      	   		OnRenewSubscriptionSuccess( agreementid, duedate, terms, total, bBillingAgreement );
-		      	   		return;
-		      	   	}
-		      	   	else
-		      	   	{
-		      	   		OnRenewSubscriptionFailure( agreementid );
-		      	   		return;
-		      	   	}
-			  	}
-			  	
-								OnRenewSubscriptionFailure( agreementid );
-		    },
-		    onFailure: function(){
-								g_bRenewSubscriptionRunning = false;
-				OnRenewSubscriptionFailure( agreementid );
-			}
-		});
-	} 
-	catch(e) 
-	{
-		ReportYourAccountJSError( 'Failed making AJAX call to RenewSubscription', e );
-	}
-}
-
-
-function OnRenewSubscriptionSuccess( agreementid, duedate, terms, total, bBillingAgreement )
-{
-	try 
-	{
-		$('transaction_due_date_'+agreementid).innerHTML = duedate;
-		if ( $('transaction_price_'+agreementid) )
-			$('transaction_price_'+agreementid).innerHTML = total;
-		$('subtext_subscription_'+agreementid).innerHTML = terms;
-		if ( bBillingAgreement )
-		{
-				$('renew_subscription_'+agreementid).innerHTML = 'Your pre-authorization of charges from %1$s has successfully been renewed.'.replace( '%1$s', $('transaction_name_'+agreementid).innerText );
-		}
-		else
-		{
-			$('renew_subscription_'+agreementid).innerHTML = 'This subscription has been renewed.';
-		}
-		new Effect.Highlight( 'renew_subscription_'+agreementid, { endcolor : '#000000', startcolor : '#ff9900' } );	
-	} 
-	catch( e ) 
-	{
-		ReportYourAccountJSError( 'Failed in OnRenewSubscriptionSuccess()', e );
-	}
-}
-
-function OnRenewSubscriptionFailure( agreementid )
-{
-	try 
-	{
-		$('renew_subscription_'+agreementid).innerHTML = 'There was a problem renewing this subscription.<br/>Please contact <a href="http://support.steampowered.com/">Steam Support</a>';
-		new Effect.Highlight( 'renew_subscription_'+agreementid, { endcolor : '#000000', startcolor : '#ff9900' } );	
 	} 
 	catch (e) 
 	{
