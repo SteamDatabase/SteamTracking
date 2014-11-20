c81b7e75eb52806c0854efa34abe70d5a7fdd006
Steam Database <freeman@steamdb.info>
2014-11-20 23:15:10 +0000

    :green_apple: M  Scripts/Community/badges.js, M  Scripts/Community/chat.js, M  Scripts/Community/discussions.js, M  Scripts/Community/global.js, M  Scripts/Community/group.js, M  Scripts/Community/profile_gameslist_functions.js, M  Scripts/shared_global.js, M  Styles/Community/global.css

--- Scripts/Community/group.js
+++ Scripts/Community/group.js
@@ -148,7 +148,7 @@ function LoadTrendingTopics()
 	var elTrendingTopics = $('group_trending_topics');
 	if ( elTrendingTopics.children.length == 0 )
 	{
-		elTrendingTopics.update('<div id="group_trending_topics_pending"><img src="https://steamcommunity-a.akamaihd.net/public/images/login/throbber.gif"></div>')
+		elTrendingTopics.update('<div id="group_trending_topics_pending"><img src="https://steamcommunity.com/public/images/login/throbber.gif"></div>')
 		g_bTrendingTopicsLoading = true;
 		new Ajax.Updater( elTrendingTopics, g_strGroupURL + '/trendingtopics', {method: 'get', onSuccess: function() { Forum_InitTooltips.defer(); } } );
 	}
