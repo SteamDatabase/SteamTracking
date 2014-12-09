eb195b3a4917d8b5376992f2d6c49d60ac98cc3e
Steam Database <freeman@steamdb.info>
2014-12-09 04:30:11 +0000

    :mega: M  Scripts/Community/economy.js

--- Scripts/Community/economy.js
+++ Scripts/Community/economy.js
@@ -2550,7 +2550,7 @@ function PopulateMarketActions( elActions, item )
 						var strInfo = '';
 						if ( transport.responseJSON.lowest_price )
 						{
-							strInfo += 'A partir de: ' + transport.responseJSON.lowest_price + '<br>'
+							strInfo += 'Starting at: ' + transport.responseJSON.lowest_price + '<br>'
 						}
 						else
 						{
