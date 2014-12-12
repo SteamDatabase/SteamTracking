1316a538e3d8f2425fe9fab241cde50fb5ba854d
Steam Database <freeman@steamdb.info>
2014-12-12 09:45:11 +0000

    :musical_score: M  Scripts/Community/economy.js

--- Scripts/Community/economy.js
+++ Scripts/Community/economy.js
@@ -2643,7 +2643,7 @@ function PopulateMarketActions( elActions, item )
 						var strInfo = '';
 						if ( transport.responseJSON.lowest_price )
 						{
-							strInfo += 'Starting at: ' + transport.responseJSON.lowest_price + '<br>'
+							strInfo += 'Prix de départ : ' + transport.responseJSON.lowest_price + '<br>'
 						}
 						else
 						{
