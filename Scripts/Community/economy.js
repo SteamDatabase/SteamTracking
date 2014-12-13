5214106e9d0594987da6668ec24cc83094c2649c
Steam Database <freeman@steamdb.info>
2014-12-13 02:15:10 +0000

    :bow: M  Scripts/Community/economy.js

--- Scripts/Community/economy.js
+++ Scripts/Community/economy.js
@@ -2643,7 +2643,7 @@ function PopulateMarketActions( elActions, item )
 						var strInfo = '';
 						if ( transport.responseJSON.lowest_price )
 						{
-							strInfo += 'Starting at: ' + transport.responseJSON.lowest_price + '<br>'
+							strInfo += 'От ' + transport.responseJSON.lowest_price + '<br>'
 						}
 						else
 						{
