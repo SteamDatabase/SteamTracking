916ce1a1bc059bf02d151fc28cdbd896dcef7c9b
Steam Database <freeman@steamdb.info>
2015-02-27 09:00:48 +0000

    :love_hotel: M  BuildbotPaths/chromehtml.txt, M  BuildbotPaths/crashhandler.txt, M  BuildbotPaths/filesystem_stdio.txt, M  BuildbotPaths/friendsui.txt, M  BuildbotPaths/gameoverlayrenderer.txt, M  BuildbotPaths/gameoverlayui.txt, M  BuildbotPaths/libaudio.txt, M  BuildbotPaths/libicui18n.txt, M  BuildbotPaths/libicuuc.txt, M  BuildbotPaths/libsteam.txt, M  BuildbotPaths/libtier0_s.txt, M  BuildbotPaths/libv8.txt, M  BuildbotPaths/libvideo.txt, M  BuildbotPaths/libvstdlib_s.txt, M  BuildbotPaths/panorama.txt, M  BuildbotPaths/serverbrowser.txt, M  BuildbotPaths/steamclient.txt, M  BuildbotPaths/steamservice.txt, M  BuildbotPaths/steamui.txt, M  BuildbotPaths/vgui2_s.txt, M  extract_client.sh

--- extract_client.sh
+++ extract_client.sh
@@ -53,7 +53,7 @@ for i in $(find bins/ -name '*.dylib');
 do
 	name=$(basename "$i" .dylib);
 	
-	strings "$i" | grep "/home/buildbot/" | sed "s/^[^\/]*\//\//" | sed "s/\:[0-9]*$//" | sort -u > "$DIR/BuildbotPaths/$name.txt"
+	strings "$i" | grep "/buildbot/" | sed "s/^[^\/]*\//\//" | sed "s/\:[0-9]*$//" | sort -u > "$DIR/BuildbotPaths/$name.txt"
 	strings "$i" -n 5 | grep "^[a-zA-Z0-9\.\_\-\%\:\/\\]*$" | grep -Evi "protobuf|GCC_except_table|home\/buildbot|[0-9]{2}:[0-9]{2}:[0-9]{2}" | c++filt -t_ | sort -u > "$DIR/ClientStrings/$name.txt"
 done
 
