019f708393d598721bb0103fb130616ed5c9b68e
Steam Database <freeman@steamdb.info>
2015-02-27 17:39:21 +0000

    Properly dump symbols

--- extract_client.sh
+++ extract_client.sh
@@ -54,15 +54,8 @@ do
 	name=$(basename "$i" .dylib);
 	
 	strings "$i" | grep "/buildbot/" | sed "s/^[^\/]*\//\//" | sed "s/\:[0-9]*$//" | sort -u > "$DIR/BuildbotPaths/$name.txt"
-	strings "$i" -n 5 | grep "^[a-zA-Z0-9\.\_\-\%\:\/\\]*$" | grep -Evi "protobuf|GCC_except_table|\/buildbot\/|[0-9]{2}:[0-9]{2}:[0-9]{2}" > temp.txt
 	
-	# -t option seg faults when on some string from steamclient.dylib
-	if [ "$name" = "steamclient" ];
-	then
-		cat temp.txt | c++filt -_ | sort -u > "$DIR/ClientStrings/$name.txt"
-	else
-		cat temp.txt | c++filt -t_ | sort -u > "$DIR/ClientStrings/$name.txt"
-	fi
+	./nm-with-macho -C -p "$i" | grep -Evi "GCC_except_table|google::protobuf" | awk '{$1=""; print $0}' | sort -u > "$DIR/Symbols/$name.txt"
 done
 
 rm temp.txt
