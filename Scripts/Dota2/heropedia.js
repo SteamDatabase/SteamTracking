a1b91106b2733b40da7d1f171978d5ec7ac53eef
Steam Database <freeman@steamdb.info>
2015-05-11 19:15:19 +0000

    :kr: M  Scripts/Dota2/heropedia.js, M  Styles/Dota2/cart.css, M  Styles/Dota2/heropedia.css, M  Styles/Dota2/itemdetails.css, M  Styles/Dota2/store.css, M  Styles/Dota2/store_featured.css

--- Scripts/Dota2/heropedia.js
+++ Scripts/Dota2/heropedia.js
@@ -174,25 +174,25 @@ function BuildItemFullBoxHTML( iData )
 		});
 
 		if ( totalPrice != 0 ) {
-			strHTML += '<div style="cursor:pointer" itemname=recipe" class="floatRecipeImage itemIconWithTooltip recipeComponent"><img src="http://cdn.dota2.com/apps/dota2/images/items/recipe_lg.png" width="49" height="37" border="0" /></div>';
+			strHTML += '<div style="cursor:pointer" itemname=recipe" class="floatRecipeImage itemIconWithTooltip recipeComponent"><img src="https://cdn.dota2.com/apps/dota2/images/items/recipe_lg.png" width="49" height="37" border="0" /></div>';
 		}
 
 		// Now, add the rest of the components
 		$.each( iData.components,
 			function( i, iName )
 			{
-				strHTML += '<div onClick="showItemInFullBox(\''+iName+'\', 1)" style="cursor:pointer" itemname="'+iName+'" class="floatRecipeImage itemIconWithTooltip recipeComponent"><img src="http://cdn.dota2.com/apps/dota2/images/items/'+iName+'_lg.png" width="49" height="37" border="0" /></div>';
+				strHTML += '<div onClick="showItemInFullBox(\''+iName+'\', 1)" style="cursor:pointer" itemname="'+iName+'" class="floatRecipeImage itemIconWithTooltip recipeComponent"><img src="https://cdn.dota2.com/apps/dota2/images/items/'+iName+'_lg.png" width="49" height="37" border="0" /></div>';
 			}
 		);
 		strHTML += '</div>';
 	}
 	strHTML += '<div class="itemName quality_'+iData.qual+'">'+iData.dname+'</div>';
-	strHTML += '<div class="goldIcon"><img src="http://cdn.dota2.com/apps/dota2/images/tooltips/gold.png" width="25" height="17" border="0" /></div>';
+	strHTML += '<div class="goldIcon"><img src="https://cdn.dota2.com/apps/dota2/images/tooltips/gold.png" width="25" height="17" border="0" /></div>';
 	strHTML += '<div class="goldCost">'+iData.cost+'</div>';
 	strHTML += '</div>';
-	strHTML += '<div class="largeItemImg"><img src="http://cdn.dota2.com/apps/dota2/images/items/'+iData.img+'" width="182" height="138" border="0" /></div>';
+	strHTML += '<div class="largeItemImg"><img src="https://cdn.dota2.com/apps/dota2/images/items/'+iData.img+'" width="182" height="138" border="0" /></div>';
 	strHTML += '<div class="itemBoxDetails">';
-	strHTML += '<div class="itemBoxHR"><img src="http://cdn.dota2.com/apps/dota2/images/heropedia/itembox_hr.png" width="646" height="1" border="0" /></div>';
+	strHTML += '<div class="itemBoxHR"><img src="https://cdn.dota2.com/apps/dota2/images/heropedia/itembox_hr.png" width="646" height="1" border="0" /></div>';
 	strHTML += '<div class="detailBoxCol detailBoxLore">'+iData.lore+'&nbsp;</div>';
 	strHTML += '<div class="detailBoxCol detailBoxDesc">'+iData.desc+'&nbsp;</div>';
 	strHTML += '<div class="detailBoxCol detailBoxAttribs">'+iData.attrib+'&nbsp;';
@@ -201,11 +201,11 @@ function BuildItemFullBoxHTML( iData )
 		strHTML += '<div class="cooldownMana">';
 		if ( iData.mc )
 		{
-			strHTML += '<div class="mana"><img alt="Mana Cost" title="Mana Cost" class="manaImg" src="http://cdn.dota2.com/apps/dota2/images/tooltips/mana.png" width="22" height="22" border="0" />'+iData.mc+'</div>';
+			strHTML += '<div class="mana"><img alt="Mana Cost" title="Mana Cost" class="manaImg" src="https://cdn.dota2.com/apps/dota2/images/tooltips/mana.png" width="22" height="22" border="0" />'+iData.mc+'</div>';
 		}
 		if ( iData.cd )
 		{
-			strHTML += '<img alt="Cooldown" title="Cooldown" class="cooldownImg" src="http://cdn.dota2.com/apps/dota2/images/tooltips/cooldown.png" width="22" height="22" border="0" />'+iData.cd+'<br clear="left" />';
+			strHTML += '<img alt="Cooldown" title="Cooldown" class="cooldownImg" src="https://cdn.dota2.com/apps/dota2/images/tooltips/cooldown.png" width="22" height="22" border="0" />'+iData.cd+'<br clear="left" />';
 		}
 		strHTML += '<br clear="left" /></div>';
 	}
