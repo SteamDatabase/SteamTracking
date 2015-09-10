4abd8f6564d51e93d69347c8a345de2b7fbe141d
Steam Database <freeman@steamdb.info>
2015-09-10 21:30:18 +0000

    :scorpius: M  ItemSchema/TeamFortress2.json, M  ItemSchema/TeamFortress2.vdf, M  Scripts/Store/dynamicstore.js

--- Scripts/Store/dynamicstore.js
+++ Scripts/Store/dynamicstore.js
@@ -13,7 +13,7 @@ function GetElemSNR( $Elem )
 	for ( var i = 0; i < links.length; ++i )
 	{
 		var link = links[i];
-		var navinfo = link.href.match( /[\?&]snr=([a-zA-Z0-9\- ]+)/ );
+		var navinfo = link.href.match( /[\?&]snr=([a-zA-Z0-9\-\_ ]+)/ );
 		if ( navinfo )
 		{
 			snr = navinfo[1];
