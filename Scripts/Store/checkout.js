ed688d1f5be3143c044485e1c777e5868716d130
Steam Database <freeman@steamdb.info>
2015-10-06 17:00:19 +0000

    :neutral_face: M  Scripts/Store/checkout.js

--- Scripts/Store/checkout.js
+++ Scripts/Store/checkout.js
@@ -1791,7 +1791,7 @@ function OnGetFinalPriceSuccess( result )
 					if ( $('col_right_review_payment_tips_header_text') && $('col_right_review_payment_tips_info_text') ) 
 					{
 						$('col_right_review_payment_tips_header_text').innerHTML = 'Tips for Trustly customers';
-						$('col_right_review_payment_tips_info_text').innerHTML = 'Complete your purchase through the BoaCompra website by signing in and completing your transaction.<br/><br/>This process can take up to five minutes.  Once you have approved payment, you will receive an email receipt confirming your purchase.';
+						$('col_right_review_payment_tips_info_text').innerHTML = 'Complete your purchase through the BoaCompra website by signing in and completing your transaction.<br/><br/>This process can take up to a few business days depending on when you complete payment.  Once you have approved payment, you will receive an email receipt confirming your purchase.';
 					}
 				}
 			}
