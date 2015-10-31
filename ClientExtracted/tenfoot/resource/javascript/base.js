218aadf74cc8b87ba466a79d684904d0f0d79d1a
Steam Database <freeman@steamdb.info>
2015-10-31 01:00:47 +0000

    :hankey: A  ClientExtracted/tenfoot/resource/images/welcomeupdates/icon_mobileapp.png, A  ClientExtracted/tenfoot/resource/images/welcomeupdates/icon_phone.png, M  ClientExtracted/tenfoot/resource/images/welcomeupdates/securitybanner.png, A  ClientExtracted/tenfoot/resource/images/welcomeupdates/securitybanner_600.png, M  ClientExtracted/tenfoot/resource/javascript/base.js, M  ClientExtracted/tenfoot/resource/layout/friends/friends_new.xml, M  ClientExtracted/tenfoot/resource/layout/library/choosebinding.xml, M  ClientExtracted/tenfoot/resource/layout/library/configbrowser.xml, M  ClientExtracted/tenfoot/resource/layout/library/configbutton.xml, M  ClientExtracted/tenfoot/resource/layout/library/library_allgames.xml, A  ClientExtracted/tenfoot/resource/layout/tips/security_sms.xml, M  ClientExtracted/tenfoot/resource/layout/tips/security_sms_twofactor.xml, A  ClientExtracted/tenfoot/resource/layout/tips/security_twofactor.xml, A  ClientExtracted/tenfoot/resource/layout/tips/tip_section_container.xml, M  ClientExtracted/tenfoot/resource/layout/welcometosteam.xml, M  ClientExtracted/tenfoot/resource/localization/tenfoot_english.txt, M  ClientExtracted/tenfoot/resource/styles/friends/friends_new.css, M  ClientExtracted/tenfoot/resource/styles/library/controllerbindings_gordon.css, M  ClientExtracted/tenfoot/resource/styles/welcometosteam.css, M  ClientManifest/steam_client_publicbeta_osx, M  ClientManifest/steam_client_publicbeta_ubuntu12, M  ClientManifest/steam_client_publicbeta_win32, M  Symbols/panorama.txt, M  Symbols/steamui.txt

--- ClientExtracted/tenfoot/resource/javascript/base.js
+++ ClientExtracted/tenfoot/resource/javascript/base.js
@@ -1,5 +1,84 @@
 /**
- * Adds/removes a class on the ancestorPanel when the given panel receives hover events
+ * Helper function to add mouseover/mouseout events when hovering over a menu or content panel
+ * which then adds/removes a class from an ancestor panel
+ */
+function SetupMenuAndContentHoverEvents( menuPanelID, menuHoverClass, contentPanelID, contentHoverClass, ancestorPanelID )
+{
+	var ancestorPanel = $( '#' + ancestorPanelID );
+
+	var hoverItemStyles = {};
+	hoverItemStyles[menuPanelID] = menuHoverClass;
+	hoverItemStyles[contentPanelID] = contentHoverClass;
+											
+	SetupPanelsHoverEvents( hoverItemStyles, ancestorPanel )													
+}
+
+
+/**
+ * for a panel on mouseover add and remove classes on a parent
+ */
+function SetupPanelHoverEvent( panel, ancestorPanel, hoverClass, uniqueClasses )
+{
+	panel.SetPanelEvent( 
+		'onmouseover', 
+		function() {
+			ancestorPanel.AddClass( hoverClass ); 
+			// now see what classes we need to remove
+			$.Each( uniqueClasses, function( classValue ) 
+			{
+				if ( classValue !=  hoverClass )
+				{
+					ancestorPanel.RemoveClass( classValue ); 				
+				}
+			} );
+		} 
+	);
+	
+	$.RegisterForUnhandledEvent( 'WindowCursorHidden', function() 
+		{
+			$.Each( uniqueClasses, function( classValue ) 
+			{
+				ancestorPanel.RemoveClass( classValue );
+			}
+		 	);
+		} );
+} 
+
+
+function onlyUnique(value, index, self) 
+{ 
+    return self.indexOf(value) === index;
+}
+
+/**
+ * Manages a map of panels and classes to use when that panel is hovered (and the other classes are removed)
+ */
+function SetupPanelsHoverEvents( hoverPanelStyleMap, ancestorPanel )
+{
+	var hoverClasses = [];
+	for( var o in hoverPanelStyleMap ) 
+	{
+	    hoverClasses.push(hoverPanelStyleMap[o]);
+	}
+
+	uniqueClasses = hoverClasses.filter( onlyUnique ); 
+	$.Each( uniqueClasses, function( classValue ) 
+	{
+		ancestorPanel.RemoveClass( classValue ); 				
+	} );
+	
+	$.Each( hoverPanelStyleMap, function( value, key ) 
+	{
+		SetupPanelHoverEvent( $( '#' + key ), ancestorPanel, hoverPanelStyleMap[key], uniqueClasses );		
+	} );
+}
+
+
+
+/**
+ * OBSOLETE - DO NOT CALL, JUST HERE FOR BACK COMPAT WITH PUBLIC WEB ASSETS 
+ *
+ *
  */
 function SetupPanelHoverEvents( panel, ancestorPanel, className )
 {
@@ -15,19 +94,4 @@ function SetupPanelHoverEvents( panel, ancestorPanel, className )
 			ancestorPanel.RemoveClass( className );
 		} 
 	);
-}
-
-
-/**
- * Helper function to add mouseover/mouseout events when hovering over a menu or content panel
- * which then adds/removes a class from an ancestor panel
- */
-function SetupMenuAndContentHoverEvents( menuPanelID, menuHoverClass, contentPanelID, contentHoverClass, ancestorPanelID )
-{
-	var menuPanel = $( '#' + menuPanelID );
-	var contentPanel = $( '#' + contentPanelID );
-	var ancestorPanel = $( '#' + ancestorPanelID );
-
-	SetupPanelHoverEvents( menuPanel, ancestorPanel, menuHoverClass );
-	SetupPanelHoverEvents( contentPanel, ancestorPanel, contentHoverClass );
 }
