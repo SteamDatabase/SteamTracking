f3f9060b621d37f6ab79164598abfcfe763d085d
Steam Database <freeman@steamdb.info>
2016-03-28 12:45:21 +0000

    :gift: M  Scripts/Community/economy.js

--- Scripts/Community/economy.js
+++ Scripts/Community/economy.js
@@ -2909,7 +2909,7 @@ function PopulateMarketActions( elActions, item )
 						var strInfo = '';
 						if ( transport.responseJSON.lowest_price )
 						{
-							strInfo += 'Starting at: ' + transport.responseJSON.lowest_price + '<br>'
+							strInfo += 'En düşük fiyat: ' + transport.responseJSON.lowest_price + '<br>'
 						}
 						else
 						{
