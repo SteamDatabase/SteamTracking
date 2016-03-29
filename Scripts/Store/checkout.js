14e34fd985677c03a0707fc783ee5b2a9ba6de02
Steam Database <freeman@steamdb.info>
2016-03-29 23:15:13 +0000

    :milky_way: M  Scripts/Store/checkout.js

--- Scripts/Store/checkout.js
+++ Scripts/Store/checkout.js
@@ -322,7 +322,7 @@ function PopupCVV2Explanation()
 			type = 'amex';
 		}
 		
-		 window.open( 'http://store.steampowered.com//cvv2explain/?webbasedpurchasing=1&type='+type, '_blank', "height=225,width=225,toolbar=no,menubar=no,resiable=no,scrollbars=no,status=no,titlebar=no" );
+		 window.open( 'http://store.steampowered.com//checkout/cvv2explain/?webbasedpurchasing=1&type='+type, '_blank', "height=225,width=225,toolbar=no,menubar=no,resiable=no,scrollbars=no,status=no,titlebar=no" );
 	} 
 	catch( e ) 
 	{
