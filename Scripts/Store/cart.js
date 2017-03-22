f7cc6e25735bc664e80a1f297c8e9717687d7cf5
Steam Database <freeman@steamdb.info>
2017-03-22 00:15:14 +0000

    3 files | M  Scripts/Store/cart.js, M  Styles/Store/cart.css, M  Styles/Store/sale.css

--- Scripts/Store/cart.js
+++ Scripts/Store/cart.js
@@ -109,3 +109,12 @@ function SaveCouponSelections( gid )
 		DismissCouponSuggestions( gid );
 }
 
+function ForgetCart()
+{
+	var date = new Date();
+	date.setTime(date.getTime()+(-10*24*60*60*1000));
+	var expires = "expires="+date.toGMTString();
+	document.cookie = 'shoppingCartGID'+"=-1; "+expires+"; path=/";
+	window.location = window.location;
+}
+
