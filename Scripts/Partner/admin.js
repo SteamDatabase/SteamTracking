3897454144f1cb04a058e98838f3232c1c3965b2
Steam Database <freeman@steamdb.info>
2017-05-12 01:30:14 +0000

    2 files | M  Random/NetworkDatagramConfig.json, M  Scripts/Partner/admin.js

--- Scripts/Partner/admin.js
+++ Scripts/Partner/admin.js
@@ -1099,11 +1099,9 @@ function LocLanguageSelect( id, language )
 
 	var textArea = document.getElementById( id + '_textarea' );
 	var hiddenInput = document.getElementById( id + language + '__hidden' );
-	// The values used here are almost always going to be stored HTML-escaped, so unescape them here to make sure they don't get double-escaped
+
 	if ( textArea && hiddenInput ) {
-		var e = document.createElement('div');
-		e.innerHTML = hiddenInput.value;
-		textArea.value = e.childNodes.length === 0 ? "" : e.childNodes[0].nodeValue;
+		textArea.value = hiddenInput.value;
 	}
 }
 
