4ee879df61026d6c7ca010b721fe045c462e56cb
Steam Database <freeman@steamdb.info>
2018-02-15 18:45:34 +0000

    2 files | M  Scripts/Store/checkout.js, M  Styles/Store/cart.css

--- Scripts/Store/checkout.js
+++ Scripts/Store/checkout.js
@@ -790,7 +790,7 @@ function OnInitializeTransactionSuccess( result )
 					|| result.paymentmethod == 18 || result.paymentmethod == 19					|| result.paymentmethod == 20 || result.paymentmethod == 21					|| result.paymentmethod == 22 || result.paymentmethod == 23					|| result.paymentmethod == 24 || result.paymentmethod == 25					|| result.paymentmethod == 26 || result.paymentmethod == 27					|| result.paymentmethod == 28 || result.paymentmethod == 29 
 					|| result.paymentmethod == 45 || result.paymentmethod == 46 
 					|| result.paymentmethod == 47 || result.paymentmethod == 121 
-					|| result.paymentmethod == 48					|| result.paymentmethod == 49 || result.paymentmethod == 50					|| result.paymentmethod == 51 || result.paymentmethod == 52					|| result.paymentmethod == 53 || result.paymentmethod == 54					|| result.paymentmethod == 55 || result.paymentmethod == 56					|| result.paymentmethod == 57 || result.paymentmethod == 58					|| result.paymentmethod == 59 || result.paymentmethod == 60					|| result.paymentmethod == 61 || result.paymentmethod == 62					|| result.paymentmethod == 66					|| result.paymentmethod == 31					|| result.paymentmethod == 34					|| result.paymentmethod == 36					|| result.paymentmethod == 37					|| result.paymentmethod == 38					|| result.paymentmethod == 65					|| result.paymentmethod == 39					|| result.paymentmethod == 40					|| result.paymentmethod == 35					|| result.paymentmethod == 67					|| result.paymentmethod == 68					|| result.paymentmethod == 69					|| result.paymentmethod == 70					|| result.paymentmethod == 71					|| result.paymentmethod == 72					|| result.paymentmethod == 73					|| result.paymentmethod == 74					|| result.paymentmethod == 75					|| result.paymentmethod == 76					|| result.paymentmethod == 77					|| result.paymentmethod == 79					|| result.paymentmethod == 81					|| result.paymentmethod == 82					|| result.paymentmethod == 83					|| result.paymentmethod == 84					|| result.paymentmethod == 85					|| result.paymentmethod == 86					|| result.paymentmethod == 87					|| result.paymentmethod == 88					|| result.paymentmethod == 89					|| result.paymentmethod == 90					|| result.paymentmethod == 91					|| result.paymentmethod == 92					|| result.paymentmethod == 93					|| result.paymentmethod == 94					|| result.paymentmethod == 95					|| result.paymentmethod == 96					|| result.paymentmethod == 97					|| result.paymentmethod == 98					|| result.paymentmethod == 99					|| result.paymentmethod == 100					|| result.paymentmethod == 101					|| result.paymentmethod == 102					|| result.paymentmethod == 103					|| result.paymentmethod == 104					|| result.paymentmethod == 105					|| result.paymentmethod == 106					|| result.paymentmethod == 107					|| result.paymentmethod == 108					|| result.paymentmethod == 109					|| result.paymentmethod == 110					|| result.paymentmethod == 111					|| result.paymentmethod == 112					|| result.paymentmethod == 113					|| result.paymentmethod == 114					|| result.paymentmethod == 115					|| result.paymentmethod == 119					|| result.paymentmethod == 120					|| result.paymentmethod == 116					|| result.paymentmethod == 117					|| result.paymentmethod == 118					|| result.paymentmethod == 63				)
+					|| result.paymentmethod == 48					|| result.paymentmethod == 49 || result.paymentmethod == 50					|| result.paymentmethod == 51 || result.paymentmethod == 52					|| result.paymentmethod == 53 || result.paymentmethod == 54					|| result.paymentmethod == 55 || result.paymentmethod == 56					|| result.paymentmethod == 57 || result.paymentmethod == 58					|| result.paymentmethod == 59 || result.paymentmethod == 60					|| result.paymentmethod == 61 || result.paymentmethod == 62					|| result.paymentmethod == 66					|| result.paymentmethod == 31					|| result.paymentmethod == 34					|| result.paymentmethod == 36					|| result.paymentmethod == 37					|| result.paymentmethod == 38					|| result.paymentmethod == 65					|| result.paymentmethod == 39					|| result.paymentmethod == 40					|| result.paymentmethod == 35					|| result.paymentmethod == 67					|| result.paymentmethod == 68					|| result.paymentmethod == 69					|| result.paymentmethod == 70					|| result.paymentmethod == 71					|| result.paymentmethod == 72					|| result.paymentmethod == 73					|| result.paymentmethod == 74					|| result.paymentmethod == 75					|| result.paymentmethod == 76					|| result.paymentmethod == 77					|| result.paymentmethod == 79					|| result.paymentmethod == 81					|| result.paymentmethod == 82					|| result.paymentmethod == 83					|| result.paymentmethod == 84					|| result.paymentmethod == 85					|| result.paymentmethod == 86					|| result.paymentmethod == 87					|| result.paymentmethod == 88					|| result.paymentmethod == 89					|| result.paymentmethod == 90					|| result.paymentmethod == 91					|| result.paymentmethod == 92					|| result.paymentmethod == 93					|| result.paymentmethod == 94					|| result.paymentmethod == 95					|| result.paymentmethod == 96					|| result.paymentmethod == 97					|| result.paymentmethod == 98					|| result.paymentmethod == 99					|| result.paymentmethod == 100					|| result.paymentmethod == 101					|| result.paymentmethod == 102					|| result.paymentmethod == 103					|| result.paymentmethod == 104					|| result.paymentmethod == 105					|| result.paymentmethod == 106					|| result.paymentmethod == 107					|| result.paymentmethod == 108					|| result.paymentmethod == 109					|| result.paymentmethod == 110					|| result.paymentmethod == 111					|| result.paymentmethod == 112					|| result.paymentmethod == 113					|| result.paymentmethod == 114					|| result.paymentmethod == 115					|| result.paymentmethod == 119					|| result.paymentmethod == 120					|| result.paymentmethod == 116					|| result.paymentmethod == 117					|| result.paymentmethod == 118					|| result.paymentmethod == 63					|| result.paymentmethod == 122				)
 		{
 						
 						$('is_external_finalize_transaction').value = 1;
@@ -1145,6 +1145,12 @@ function OnGetFinalPriceSuccess( result )
 			}
 		
 			var method = $('payment_method');
+// hackhack
+if ( method.value == 'cafefunded' )
+{
+	result.externalurl = 'http://kurtisc3.valve.org/store/checkout/cafefundedinstruction';
+	result.useexternalredirect = true;
+}			
 			if ( result.externalurl )
 			{
 								var url = result.externalurl.replace( /%/g, '%25' );
@@ -2971,7 +2977,7 @@ function UpdatePaymentInfoForm()
 		{
 			bShowAddressForm = false;
 			bShowCountryVerification = true;
-		bShowBankSelection = true;
+			bShowBankSelection = true;
 		}
 		else if ( method.value == 'paysafe' || method.value == 'sofort' || method.value == 'webmoney' || method.value == 'moneybookers'	|| method.value == 'alipay' || method.value == 'unionpay' || method.value == 'yandex'
 			|| method.value == 'boacompragold' || method.value == 'pagseguro' || method.value == 'visabrazil'
@@ -4298,7 +4304,7 @@ function UpdateReviewPageBillingInfoWithCurrentValues( price_data )
 			}
 			else if ( method.value == 'cafefunded' && providerPaymentMethod == 122 )
 			{
-				$('payment_method_review_text').innerHTML = 'Cafe';
+				$('payment_method_review_text').innerHTML = 'In Person via Cafe: ' + sSiteName;
 				$('checkout_review_payment_info_area').style.display = 'none';
 			}			
 		}
@@ -4812,6 +4818,13 @@ function DisplayReceiptPage()
 	$('cart_area').style.display = 'none';
 	$('receipt_area').style.display = 'block';
 	$('pending_receipt_area').style.display = 'none';
+	
+	var method = $('payment_method');
+	if ( method.value == 'cafefunded' && g_winExternal )
+	{
+		g_winExternal.close();
+		g_winExternal = false;
+	}
 
 	if ( $('checkout_pipeline') && $('receipt_pipeline') )
 	{
@@ -5091,7 +5104,7 @@ function PollForTransactionStatus( txnid, retries, timeout )
 	      		
 			      		g_bFinalizeTransactionInProgress = false;
 			      		g_bPollingForTransactionStatus = false;
-					g_timeoutPoll = false;
+								g_timeoutPoll = false;
 					
 			      	   				      	   	if ( result.success == 1 || bNeedsApproval )
 		      		   	{
