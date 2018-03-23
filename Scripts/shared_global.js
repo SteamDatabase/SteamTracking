6041bc33f3a76a32b44c2701b5717dcd159545c4
Steam Database <freeman@steamdb.info>
2018-03-23 06:00:16 +0000

    1 files | M  Scripts/shared_global.js

--- Scripts/shared_global.js
+++ Scripts/shared_global.js
@@ -4107,6 +4107,7 @@ CAjaxSubPageController.prototype.InstrumentLinks = function( elTarget )
 		rgLinks[i].addEventListener('click', this.Navigate.bind(this, rgLinks[i].dataset[ this.strStateID ], rgLinks[i].dataset[ 'title' ] ) );
 		rgLinks[i].href = this.strBaseURL + rgLinks[i].dataset[ this.strStateID ];
 	}
+	BindTooltips( elTarget );
 }
 
 /**
@@ -4211,8 +4212,8 @@ CAjaxSubPageController.prototype.OnWindowPopState = function( event )
 function BindTooltips(selector, rgOptions)
 {
 	// Standard tooltips
-	$J( '[data-tooltip-text]', selector).v_tooltip( { 'tooltipClass': rgOptions.tooltipCSSClass, 'dataName': 'tooltipText', 'defaultType': 'text', 'replaceExisting': false } );
-	$J( '[data-tooltip-html]', selector).v_tooltip( { 'tooltipClass': rgOptions.tooltipCSSClass, 'dataName': 'tooltipHtml', 'defaultType': 'html', 'replaceExisting': false } );
+	$J( '[data-tooltip-text]', selector).v_tooltip( { 'tooltipClass': rgOptions.tooltipCSSClass, 'dataName': 'tooltipText', 'defaultType': 'text' } );
+	$J( '[data-tooltip-html]', selector).v_tooltip( { 'tooltipClass': rgOptions.tooltipCSSClass, 'dataName': 'tooltipHtml', 'defaultType': 'html' } );
 }
 
 /**
