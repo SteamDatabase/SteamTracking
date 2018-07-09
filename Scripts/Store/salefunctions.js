52d74afa66a26ffda862522c1c36de8373a93895
Steam Database <freeman@steamdb.info>
2018-07-09 20:15:46 +0000

    4 files | M  Scripts/Store/home.js, M  Scripts/Store/salefunctions.js, M  Styles/Store/home.css, M  Styles/Store/store.css

--- Scripts/Store/salefunctions.js
+++ Scripts/Store/salefunctions.js
@@ -88,7 +88,6 @@ function AddItemsIfNotPresent( rgItemsToDisplay, rgItemsFound, cMaxItems )
 	}
 }
 
-
 function HomeRenderFeaturedItems( rgDisplayLists )
 {
 	var rgTier1 = [];
@@ -582,114 +581,6 @@ function FillCapsuleContainer( rgItems, $Parent, settings )
 		$Parent.hide();
 	}
 }
-
-GSteamBroadcasts = {
-	Init: function()
-	{
-		GSteamBroadcasts.Render();
-	},
-
-	Render: function()
-	{
-		if ($J('.live_streams_ctn').length == 0 )
-			return;
-
-		$J('.live_streams_ctn').hide();
-			
-		// do an ajax call to get the broadcasters
-		$J.ajax( {
-			url: "https:\/\/store.steampowered.com\/broadcast\/ajaxgetpopularpartnerbroadcasts\/",
-			data: {
-				'minviews' : 1,
-			},
-			dataType: 'json',
-			type: 'GET'
-		}).done(function( data ) {
-
-			if ( data.success == 1 && data.filtered.length > 0 )
-			{
-				for( var i = 0; i < data.filtered.length; i++ )
-				{
-					var rgAppInfo = GStoreItemData.rgAppData[ data.filtered[i].appid ];
-					if ( rgAppInfo )
-					{
-						rgAppInfo.live_broadcast = true;
-						
-						// update all broadcast capsules to have live tag
-						var $Capsules = $J('.broadcast_capsule[data-ds-appid=' + data.filtered[i].appid + ']' ).each( function()
-						{
-							$Icon = $J('<div/>', {'class': 'broadcast_live_stream_icon' } ).append( 'Live');
-							$J(this).append( $Icon );
-						});
-					}
-				}
-				
-				GStoreItemData.AddStoreItemData( data.rgAppData );
-				
-				var rgFiltered = GHomepage.FilterItemsForDisplay(
-					data.filtered, 'home', 1, 6, { displayed_elsewhere: true }
-				);
-
-				var rgFiltered2 = [];
-				for ( var i = 0; i < rgFiltered.length; i++ )
-				{
-					if ( rgFiltered[i].popular )
-						rgFiltered2.push( rgFiltered[i] );
-				}
-
-				if ( rgFiltered2.length > 0 )
-				{
-					$elTarget = $J('#live_streams_carousel');
-					var $elCapsuleTarget = $J('.carousel_items', $elTarget);
-				
-					var $elPageContainer = $J('<div>', { 'class': 'focus' } );
-
-					for ( var i = 0; i < rgFiltered2.length && i < 6; i++ )
-					{
-						var oItem = rgFiltered2[i];
-			
-						var params = { 'class': 'store_capsule broadcast_capsule', 'href': oItem.app_link };
-					
-						var rgItemData = GStoreItemData.GetCapParams( 'summer2018_live_stream', oItem.appid, 0, null, params );
-						var $CapCtn = $J('<a/>', params );
-
-						var $ImgCtn = $J('<div class="capsule headerv5"/>');
-
-						var rgImageProperties = { src: oItem.thumbnail_http_address };
-						var rgDefaultImageProperties = { src: oItem.app_capsule_image };
-
-						$ImgCtn.append( $J('<img/>', rgDefaultImageProperties ) );
-						$ImgCtn.append( $J('<img/>', rgImageProperties ) );
-						$CapCtn.append( $ImgCtn );
-					
-						var rgPlayIconProperties = { src: 'https://steamstore-a.akamaihd.net/public/shared/images/apphubs/play_icon80.png', class: "live_stream_play_icon" };
-						$ImgCtn.append( $J('<img/>', rgPlayIconProperties ) );
-					
-						$Contents = $J('<div/>', {'class': 'title ellipsis' } );
-						$Contents.append( $J('<span/>').html( oItem.app_name ) );
-						$Contents.append( $J('<span/>', {'class': 'live_steam_viewers' } ).html( oItem.viewer_count ) );
-					
-						$CapCtn.append( $Contents );
-						$CapCtn.append( 
-								$J('<div/>', {'class': 'broadcast_live_stream_icon' } ).append( 'Live')
-						);
-					
-
-						$elPageContainer.append( $CapCtn );
-					}
-				
-					$elCapsuleTarget.append($elPageContainer);
-
-					$J('.live_streams_ctn').show();
-				}
-			}
-
-		});
-		
-		return;
-	}
-};
-
 
 GSteamSalienPlanets = {
 	Init: function()
