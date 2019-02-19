932636731771a891dcd67099cd146fb9e0bdedce
Steam Database <freeman@steamdb.info>
2019-02-19 01:05:01 +0000

    6 files | M  BuildbotPaths/steamclient.txt, M  ClientManifest/steam_client_publicbeta_osx, M  ClientManifest/steam_client_publicbeta_ubuntu12, M  ClientManifest/steam_client_publicbeta_win32, M  Structs/ISteamNetworkingUtils001.h, M  Symbols/steamclient.txt

--- Structs/ISteamNetworkingUtils001.h
+++ Structs/ISteamNetworkingUtils001.h
@@ -18,4 +18,8 @@ public:
     virtual unknown_ret GetConfigValue(ESteamNetworkingConfigValue, ESteamNetworkingConfigScope, long, ESteamNetworkingConfigDataType*, void*, unsigned long*) = 0;
     virtual unknown_ret GetConfigValueInfo(ESteamNetworkingConfigValue, char const**, ESteamNetworkingConfigDataType*, ESteamNetworkingConfigScope*, ESteamNetworkingConfigValue*) = 0;
     virtual unknown_ret GetFirstConfigValue() = 0;
+    virtual unknown_ret SteamNetworkingIPAddr_ToString(SteamNetworkingIPAddr const&, char*, unsigned long, bool) = 0;
+    virtual unknown_ret SteamNetworkingIPAddr_ParseString(SteamNetworkingIPAddr*, char const*) = 0;
+    virtual unknown_ret SteamNetworkingIdentity_ToString(SteamNetworkingIdentity const&, char*, unsigned long) = 0;
+    virtual unknown_ret SteamNetworkingIdentity_ParseString(SteamNetworkingIdentity*, char const*) = 0;
 };
