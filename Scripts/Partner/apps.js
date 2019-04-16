877749b027701c4b6e9b0d6eb8951022c82ecfc9
Steam Database <freeman@steamdb.info>
2019-04-16 23:04:32 +0000

    3 files | M  Random/NetworkDatagramConfig.json, M  Scripts/Partner/apps.js, M  Styles/Partner/styles_global.css

--- Scripts/Partner/apps.js
+++ Scripts/Partner/apps.js
@@ -2195,6 +2195,24 @@ function RichPresenceLocCallbackClosure( appid )
 	return theClosure;
 }
 
+function BroadcastGameDataLocCallbackClosure( appid )
+{
+	theClosure = function( jsonResponse )
+	{
+		// the json is wrapped in a <pre> tag, so we must unwrap it.
+		var div = new Element("div");
+		console.log( jsonResponse );
+		div.innerHTML = jsonResponse;
+		var results = div.childElements()[0].innerHTML.evalJSON(true);
+
+		// poke in results
+		StandardCallback( results, 'game_data_upload_response' );
+		return false;
+	};
+
+	return theClosure;
+}
+
 function AchievementImageUploadCallbackClosure( appid, statid, bitid, gray )
 {
 	theClosure = function( jsonResponse )
