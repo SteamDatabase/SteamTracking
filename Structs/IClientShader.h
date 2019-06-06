9726d5e055db96cfdb35bcbd315a90729d10ac3c
Steam Database <freeman@steamdb.info>
2019-06-06 02:05:25 +0000

    20 files | M  BuildbotPaths/steamclient.txt, D  ClientExtracted/singleapplauncher/graphics/download_progress_back.png, D  ClientExtracted/singleapplauncher/graphics/launch_banner.png, D  ClientExtracted/singleapplauncher/graphics/launch_button.png, D  ClientExtracted/singleapplauncher/graphics/launch_button_active.png, D  ClientExtracted/singleapplauncher/graphics/launch_button_disabled.png, D  ClientExtracted/singleapplauncher/graphics/launch_button_hover.png, D  ClientExtracted/singleapplauncher/resource/singleapplauncher_english.txt, M  ClientExtracted/tenfoot/resource/localization/tenfoot_english.txt, M  ClientManifest/steam_client_publicbeta_osx, M  ClientManifest/steam_client_publicbeta_ubuntu12, M  ClientManifest/steam_client_publicbeta_win32, M  Protobufs/steammessages_clientserver.proto, A  Protobufs/steammessages_lobbymatchmaking.steamclient.proto, M  Protobufs/steammessages_virtualcontroller.proto, M  Protobufs/stream.proto, M  Structs/IClientShader.h, M  Structs/emsg_list.json, M  Symbols/steamclient.txt, M  Symbols/steamui.txt

--- Structs/IClientShader.h
+++ Structs/IClientShader.h
@@ -5,6 +5,7 @@ public:
     virtual unknown_ret EnableShaderManagement(bool) = 0;
     virtual unknown_ret GetShaderDepotsTotalDiskUsage() = 0;
     virtual unknown_ret StartShaderScan(unsigned int, char const*) = 0;
+    virtual unknown_ret StartPipelineBuild(unsigned int, int) = 0;
     virtual unknown_ret ProcessShaderCache(unsigned int) = 0;
     virtual unknown_ret SetupShaderCacheEnvironment(char*, unsigned int) = 0;
     virtual unknown_ret BAppHasPendingShaderContentDownload(unsigned int) = 0;
