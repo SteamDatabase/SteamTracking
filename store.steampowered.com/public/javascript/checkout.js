081a372ae8bbf105f7a9acec4a8ca06b7157cb29
Steam Database <freeman@steamdb.info>
2019-10-11 01:04:12 +0000

    1 files | checkout.js

--- store.steampowered.com/public/javascript/checkout.js
+++ store.steampowered.com/public/javascript/checkout.js
@@ -936,7 +936,10 @@ function OnInitializeTransactionFailure( detail, result )
 					error_text = 'This payment method is currently unavailable for use.  We are working to resolve the issue.  Please select another payment method for your purchase and try again.';
 					break;
 				case 83:
-					error_text = 'The selected payment method is not supported for a product you are purchasing.  Please select another payment method for your purchase and try again.';
+					if ( result.paymentmethod == 128 )
+						error_text = 'The selected payment method is temporarily unavailable for a product you are purchasing.  Please select another payment method to continue your purchase now or try your purchase at a later time.';
+					else
+						error_text = 'The selected payment method is not supported for a product you are purchasing.  Please select another payment method for your purchase and try again.';
 					break;
 				case 33:
 					error_text = 'Your purchase could not be completed because your credit card has expired. Please enter a new credit card and try again.';
