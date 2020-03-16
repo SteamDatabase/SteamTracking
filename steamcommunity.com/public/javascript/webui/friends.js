204f75e17d7dd06fc7fa75cf5a960732247beb9b
Steam Database <freeman@steamdb.info>
2020-03-16 21:04:25 +0000

    6 files | friends.css, chathelpers.js, clientcom.js, friends.js, profile.js, steammessages.js

--- steamcommunity.com/public/javascript/webui/friends.js
+++ steamcommunity.com/public/javascript/webui/friends.js
@@ -2,7 +2,7 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "5754280";
+var CLSTAMP = "5757552";
 !(function(e) {
   function t(t) {
     for (
