b9145f353e9764223496982b3ce44d7031cfe0ae
Steam Database <freeman@steamdb.info>
2020-05-01 17:44:02 +0000

    9 files | steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, gameoverlayui.txt, panorama.txt, steamservice.txt, steamui.txt, IClientNetworkingUtilsSerialized.h, steamclient.txt

--- Structs/IClientNetworkingUtilsSerialized.h
+++ Structs/IClientNetworkingUtilsSerialized.h
@@ -2,5 +2,6 @@ class IClientNetworkingUtilsSerialized
 {
 public:
     virtual unknown_ret GetNetworkConfigJSON(void*, unsigned int, char const*) = 0;
+    virtual unknown_ret GetLauncherType() = 0;
     virtual unknown_ret TEST_ClearCachedNetworkConfig() = 0;
 };
