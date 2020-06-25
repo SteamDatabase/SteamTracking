8b8b2957226e838572bb38b73186fccb95e61cc4
Steam Database <freeman@steamdb.info>
2020-06-25 23:04:14 +0000

    2 files | main.css, main.js

--- steamcommunity.com/public/javascript/applications/community/main.js
+++ steamcommunity.com/public/javascript/applications/community/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "5950679";
+var CLSTAMP = "5952018";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [91],
   {
