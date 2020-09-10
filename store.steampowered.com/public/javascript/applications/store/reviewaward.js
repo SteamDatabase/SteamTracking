d0c0f8804156a06f8a548c3595391242e000d571
Steam Database <freeman@steamdb.info>
2020-09-10 18:04:13 +0000

    1 files | reviewaward.js

--- store.steampowered.com/public/javascript/applications/store/reviewaward.js
+++ store.steampowered.com/public/javascript/applications/store/reviewaward.js
@@ -464,19 +464,13 @@
                 (m = r),
                 (p = []),
                 b.forEach(function(e) {
-                  if (
-                    e.valid_target_types.find(function(e) {
-                      return e == u;
-                    })
-                  )
+                  if (e.valid_target_types.includes(u))
                     switch (u) {
                       case 1:
                         p.push(e.reactionid);
                         break;
                       case 2:
-                        e.valid_ugc_types.find(function(e) {
-                          return e == m;
-                        }) && p.push(e.reactionid);
+                        e.valid_ugc_types.includes(m) && p.push(e.reactionid);
                     }
                 }),
                 p),
