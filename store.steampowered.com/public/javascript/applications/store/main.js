e55451395a65046a524626949e9068ec9ebba12a
Steam Database <freeman@steamdb.info>
2020-12-22 21:04:29 +0000

    4 files | loyaltystore.css, main.css, main.js, manifest.js

--- store.steampowered.com/public/javascript/applications/store/main.js
+++ store.steampowered.com/public/javascript/applications/store/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6285575";
+var CLSTAMP = "6285624";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [9],
   {
--- store.steampowered.com/public/javascript/applications/store/manifest.js
+++ store.steampowered.com/public/javascript/applications/store/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6285575";
+var CLSTAMP = "6285624";
 !(function(o) {
   function e(e) {
     for (
@@ -125,7 +125,7 @@ var CLSTAMP = "6285575";
                     5: "31d6cfe0d16ae931b73c",
                     6: "af17ac0a598cb32e8ba3",
                     7: "ec433dd8cfb12ab117d0",
-                    8: "9c57fd6650388d43b765",
+                    8: "38e47b37414371e46603",
                     10: "31d6cfe0d16ae931b73c",
                     11: "31d6cfe0d16ae931b73c",
                     12: "31d6cfe0d16ae931b73c",
