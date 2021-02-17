2d1524df33db4542a1e1c9d9ee3670ff6d3d0daa
Steam Database <freeman@steamdb.info>
2021-02-17 02:04:34 +0000

    7 files | friends.css, main.js, manifest.js, chathelpers.js, clientcom.js, friends.js, steammessages.js

--- steamcommunity.com/public/javascript/applications/community/main.js
+++ steamcommunity.com/public/javascript/applications/community/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6372599";
+var CLSTAMP = "6372764";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [68],
   {
