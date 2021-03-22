480290aad6422c9a6a3f1abeadc335d23594e9dc
Steam Database <freeman@steamdb.info>
2021-03-22 23:04:49 +0000

    14 files | csgo_launcher_win32, dota2_launcher_win32, steam_client_osx, steam_client_ubuntu12, steam_client_win32, steam_cmd_linux, steam_cmd_osx, steam_cmd_win32, steamchina_win32, friends.css, chathelpers.js, clientcom.js, friends.js, steammessages.js

--- steamcommunity.com/public/javascript/webui/clientcom.js
+++ steamcommunity.com/public/javascript/webui/clientcom.js
@@ -2,7 +2,7 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6431868";
+var CLSTAMP = "6432754";
 !(function (t) {
   var e = {};
   function n(r) {
