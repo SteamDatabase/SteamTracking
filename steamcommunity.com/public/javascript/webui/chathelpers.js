fd46db50f60059dc390d128d578ff0125f326ecf
Steam Database <freeman@steamdb.info>
2021-03-25 20:04:52 +0000

    6 files | friends.css, chathelpers.js, clientcom.js, friends.js, steammessages.js, dota_react.css

--- steamcommunity.com/public/javascript/webui/chathelpers.js
+++ steamcommunity.com/public/javascript/webui/chathelpers.js
@@ -2,7 +2,7 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6444101";
+var CLSTAMP = "6444661";
 !(function (e) {
   function t(t) {
     for (
--- steamcommunity.com/public/javascript/webui/steammessages.js
+++ steamcommunity.com/public/javascript/webui/steammessages.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6444101";
+var CLSTAMP = "6444661";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [2],
   {
