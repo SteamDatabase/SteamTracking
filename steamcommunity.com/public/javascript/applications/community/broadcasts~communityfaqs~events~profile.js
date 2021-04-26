4c7be3bcb4c0c33c870cb565d0f2bd14e92e7d59
Steam Database <freeman@steamdb.info>
2021-04-26 15:04:21 +0000

    1 files | broadcasts~communityfaqs~events~profile.js

--- steamcommunity.com/public/javascript/applications/community/broadcasts~communityfaqs~events~profile.js
+++ steamcommunity.com/public/javascript/applications/community/broadcasts~communityfaqs~events~profile.js
@@ -18081,7 +18081,7 @@
                         return (
                           (R = a.sent()),
                           (k = L.length),
-                          (null == _ ? void 0 : _.BIsAnyUserPreferenceEnabled())
+                          _.BIsAnyUserPreferenceEnabled()
                             ? ((G = _.BIsUserPreferenceEnabled(
                                 u.n.k_EHideOwnedItems
                               )),
