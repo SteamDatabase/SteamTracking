f931be0af34b3cafe8a1f222989cd3b121f3cf28
Steam Database <freeman@steamdb.info>
2021-06-07 12:04:21 +0000

    2 files | broadcasts~communityfaqs~events~profile.css, steammessages.js

--- steamcommunity.com/public/javascript/webui/steammessages.js
+++ steamcommunity.com/public/javascript/webui/steammessages.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6575755";
+var CLSTAMP = "6576413";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [2],
   {
