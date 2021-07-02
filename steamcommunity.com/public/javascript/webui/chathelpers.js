bb67294a998858580d75a0368ce772b6453a90ad
Steam Database <freeman@steamdb.info>
2021-07-02 11:04:18 +0000

    1 files | chathelpers.js

--- steamcommunity.com/public/javascript/webui/chathelpers.js
+++ steamcommunity.com/public/javascript/webui/chathelpers.js
@@ -2,7 +2,7 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6645000";
+var CLSTAMP = "6634917";
 !(function (e) {
   function t(t) {
     for (
