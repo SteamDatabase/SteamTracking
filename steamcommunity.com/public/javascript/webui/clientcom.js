499460edbf22962bdc8a3c2a9568d043d5097d78
Steam Database <freeman@steamdb.info>
2021-08-26 02:04:21 +0000

    6 files | main.css, communityfaqs~events.js, conference.js, main_english.json, clientcom.js, main_english.json

--- steamcommunity.com/public/javascript/webui/clientcom.js
+++ steamcommunity.com/public/javascript/webui/clientcom.js
@@ -2,7 +2,7 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6730723";
+var CLSTAMP = "6741575";
 !(function (t) {
   var e = {};
   function n(r) {
