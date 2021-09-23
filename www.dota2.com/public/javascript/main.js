42b1f8ae8b0581c89a2420d031d88d119e844f42
Steam Database <freeman@steamdb.info>
2021-09-23 19:05:23 +0000

    4 files | steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, main.js

--- www.dota2.com/public/javascript/main.js
+++ www.dota2.com/public/javascript/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6782269";
+var CLSTAMP = "6785651";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [1],
   {
