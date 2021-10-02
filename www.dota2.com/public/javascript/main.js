582b966d6124c355d430d2658c16abaf8e3c8a08
Steam Database <freeman@steamdb.info>
2021-10-02 04:04:22 +0000

    1 files | main.js

--- www.dota2.com/public/javascript/main.js
+++ www.dota2.com/public/javascript/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6804599";
+var CLSTAMP = "6805247";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [1],
   {
