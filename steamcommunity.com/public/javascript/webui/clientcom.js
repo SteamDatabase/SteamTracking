be20675d7a4c96d748e3f6ad9090d5aed2910646
Steam Database <freeman@steamdb.info>
2021-10-13 16:04:18 +0000

    2 files | chathelpers.js, clientcom.js

--- steamcommunity.com/public/javascript/webui/clientcom.js
+++ steamcommunity.com/public/javascript/webui/clientcom.js
@@ -2,7 +2,7 @@
 
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6826072";
+var CLSTAMP = "6810750";
 !(function (t) {
   var e = {};
   function n(r) {
