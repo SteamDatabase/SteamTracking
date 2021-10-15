371205a701dda032b98697a11d76e3f94e980ecc
Steam Database <freeman@steamdb.info>
2021-10-15 04:04:22 +0000

    1 files | steammessages.js

--- steamcommunity.com/public/javascript/webui/steammessages.js
+++ steamcommunity.com/public/javascript/webui/steammessages.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6810750";
+var CLSTAMP = "6832119";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [2],
   {
