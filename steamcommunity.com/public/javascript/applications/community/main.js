d944b9382c51a37ba06a128fb2204fcf13c55546
Steam Database <freeman@steamdb.info>
2021-10-21 22:04:45 +0000

    9 files | friends.css, main.js, manifest.js, chathelpers.js, clientcom.js, friends.js, steammessages.js, store.css, main.js

--- steamcommunity.com/public/javascript/applications/community/main.js
+++ steamcommunity.com/public/javascript/applications/community/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6844929";
+var CLSTAMP = "6845388";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [101],
   {
--- www.dota2.com/public/javascript/main.js
+++ www.dota2.com/public/javascript/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6840915";
+var CLSTAMP = "6845154";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [1],
   {
