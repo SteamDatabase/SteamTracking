3e6dd2d39ce5d8fd6137b51ee6e5c35e306cfceb
Steam Database <freeman@steamdb.info>
2021-11-02 20:50:56 +0000

    35 files | libavcodec.58.txt, libavformat.58.txt, libavresample.4.txt, libavutil.56.txt, libswscale.5.txt, libraries.js, libraries_cm.js, library.js, libraryroot.js, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steamchina_publicbeta_osx, steamchina_publicbeta_win32, steamnetworkingsockets_messages.proto, crashhandler.txt, gameoverlayrenderer.txt, gameoverlayui.txt, libaudio.txt, libavcodec.58.txt, libavformat.58.txt, libavresample.4.txt, libavutil.56.txt, libswscale.5.txt, libtier0_s.txt, libvideo.txt, libvstdlib_s.txt, panorama.txt, steamclient.txt, steamconsole.txt, steamservice.txt, steamui.txt

--- ClientExtracted/steamui/libraries/libraries.js
+++ ClientExtracted/steamui/libraries/libraries.js
@@ -1,5 +1,5 @@
 /* Third-party software licenses can be found at licenses.txt */
-var CLSTAMP = "6861042";
+var CLSTAMP = "6865241";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [0],
   {
