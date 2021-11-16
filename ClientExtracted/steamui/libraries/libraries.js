b98a0577d1eb603362b8f345791e6c4c3971f92d
Steam Database <freeman@steamdb.info>
2021-11-16 22:05:30 +0000

    109 files | shared_l3.svg, shared_l3_lg.png, shared_l3_md.png, shared_l3_sm.png, shared_lstick.svg, shared_lstick_click.svg, shared_lstick_click_lg.png, shared_lstick_click_md.png, shared_lstick_click_sm.png, shared_lstick_lg.png, shared_lstick_md.png, shared_lstick_sm.png, shared_r3.svg, shared_r3_lg.png, shared_r3_md.png, shared_r3_sm.png, shared_rstick.svg, shared_rstick_click.svg, shared_rstick_click_lg.png, shared_rstick_click_md.png, shared_rstick_click_sm.png, shared_rstick_lg.png, shared_rstick_md.png, shared_rstick_sm.png, shared_rstick_touch.svg, shared_rstick_touch_lg.png, shared_rstick_touch_md.png, shared_rstick_touch_sm.png, shared_l3.svg, shared_l3_lg.png, shared_l3_md.png, shared_l3_sm.png, shared_lstick.svg, shared_lstick_click.svg, shared_lstick_click_lg.png, shared_lstick_click_md.png, shared_lstick_click_sm.png, shared_lstick_lg.png, shared_lstick_md.png, shared_lstick_sm.png, shared_r3.svg, shared_r3_lg.png, shared_r3_md.png, shared_r3_sm.png, shared_rstick.svg, shared_rstick_click.svg, shared_rstick_click_lg.png, shared_rstick_click_md.png, shared_rstick_click_sm.png, shared_rstick_lg.png, shared_rstick_md.png, shared_rstick_sm.png, shared_rstick_touch.svg, shared_rstick_touch_lg.png, shared_rstick_touch_md.png, shared_rstick_touch_sm.png, shared_l3.svg, shared_l3_lg.png, shared_l3_md.png, shared_l3_sm.png, shared_lstick.svg, shared_lstick_click.svg, shared_lstick_click_lg.png, shared_lstick_click_md.png, shared_lstick_click_sm.png, shared_lstick_lg.png, shared_lstick_md.png, shared_lstick_sm.png, shared_r3.svg, shared_r3_lg.png, shared_r3_md.png, shared_r3_sm.png, shared_rstick.svg, shared_rstick_click.svg, shared_rstick_click_lg.png, shared_rstick_click_md.png, shared_rstick_click_sm.png, shared_rstick_lg.png, shared_rstick_md.png, shared_rstick_sm.png, shared_rstick_touch.svg, shared_rstick_touch_lg.png, shared_rstick_touch_md.png, shared_rstick_touch_sm.png, 6.css, libraries.js, libraries_cm.js, library.js, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steamchina_publicbeta_osx, steamchina_publicbeta_win32, crashhandler.txt, gameoverlayrenderer.txt, gameoverlayui.txt, libaudio.txt, libtier0_s.txt, libvideo.txt, libvstdlib_s.txt, panorama.txt, steamclient.txt, steamservice.txt, steamui.txt, libtier0_s.txt, libvideo.txt

--- ClientExtracted/steamui/libraries/libraries.js
+++ ClientExtracted/steamui/libraries/libraries.js
@@ -1,5 +1,5 @@
 /* Third-party software licenses can be found at licenses.txt */
-var CLSTAMP = "6887637";
+var CLSTAMP = "6893938";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [0],
   {
