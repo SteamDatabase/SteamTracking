bf5f2b1110da8e5d013c8e1ae692b194279b00c2
Steam Database <freeman@steamdb.info>
2021-12-07 22:04:21 +0000

    1 files | main.js

--- www.dota2.com/public/javascript/main.js
+++ www.dota2.com/public/javascript/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "6934368";
+var CLSTAMP = "6934502";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [1],
   {
@@ -10352,7 +10352,7 @@ var CLSTAMP = "6934368";
                     eRegion: u.k.EEU,
                     eDivision: u.d.DIVISION_II,
                     nLeagueID: 13710,
-                    arrPhaseInfos: [{ ePhase: u.f.OVERALL, nNodeGroupID: 2 }],
+                    arrPhaseInfos: [{ ePhase: u.f.OVERALL, nNodeGroupID: 4 }],
                   },
                   {
                     eRegion: u.k.CN,
