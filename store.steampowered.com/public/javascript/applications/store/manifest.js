83c1ddb47769b0a5f2a4b7f2e7229fa19f139df8
Steam Database <freeman@steamdb.info>
2022-02-15 19:04:31 +0000

    4 files | loyaltystore.css, main.js, manifest.js, main.js

--- store.steampowered.com/public/javascript/applications/store/manifest.js
+++ store.steampowered.com/public/javascript/applications/store/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "7077705";
+var CLSTAMP = "7083026";
 !(function (e) {
   function a(a) {
     for (
@@ -206,7 +206,7 @@ var CLSTAMP = "7077705";
                     37: "31d6cfe0d16ae931b73c",
                     38: "31d6cfe0d16ae931b73c",
                     39: "31d6cfe0d16ae931b73c",
-                    40: "626d6236da7063d04b0a",
+                    40: "ae69faf5788ffafc6093",
                     42: "31d6cfe0d16ae931b73c",
                     43: "31d6cfe0d16ae931b73c",
                     44: "31d6cfe0d16ae931b73c",
