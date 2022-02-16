e57f33a3c8b960807dcaffb0808cb27ae0ee0528
Steam Database <freeman@steamdb.info>
2022-02-16 02:04:27 +0000

    2 files | main.js, manifest.js

--- help.steampowered.com/public/javascript/applications/help/manifest.js
+++ help.steampowered.com/public/javascript/applications/help/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "7084249";
+var CLSTAMP = "7084411";
 !(function (d) {
   function a(a) {
     for (
