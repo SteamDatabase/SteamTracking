1b0c6b890beea6c219327cca1843391e0b5a1071
Pavel Djundik <xPaw@users.noreply.github.com>
2022-02-27 22:35:49 +0200

    Implement reading fields

--- dump_javascript_protobufs.mjs
+++ dump_javascript_protobufs.mjs
@@ -10,22 +10,54 @@ for (const file of files) {
 	try {
 		const code = fs.readFileSync(file);
 		const ast = parse(code, { ecmaVersion: latestEcmaVersion, loc: true });
+		const messages = [];
 
 		traverse(ast, {
 			enter: function (node) {
-				if (node.type == Syntax.Property) {
-					TraverseModule(node.value);
+				if (node.type === Syntax.Property) {
+					messages.push(...TraverseModule(node.value));
 					this.skip();
 				}
 			},
 		});
+
+		OutputMessages(messages);
 	} catch (e) {
-		console.error(`Unable to parse "${path.basename(file)}": ${e}`);
+		console.error(`Unable to parse "${path.basename(file)}":`, e);
 		continue;
 	}
 }
 
+function OutputMessages(messages, stream = process.stdout) {
+	for (const message of messages) {
+		stream.write(`message ${message.className} {\n`);
+
+		for (const field of message.fields) {
+			stream.write(`\t${field.flag} ${field.type} ${field.name} = ${field.id}`);
+
+			let options = [];
+
+			if (Object.hasOwn(field, "default")) {
+				options.push(`default = ${field.default}`);
+			}
+
+			if (Object.hasOwn(field, "description")) {
+				options.push(`(description) = "${field.description}"`);
+			}
+
+			if (options.length > 0) {
+				stream.write(` [${options.join(", ")}]`);
+			}
+
+			stream.write(";\n");
+		}
+
+		stream.write("}\n\n");
+	}
+}
+
 function TraverseModule(ast) {
+	const messages = [];
 	let messageIdentifier = null;
 
 	traverse(ast, {
@@ -40,26 +72,32 @@ function TraverseModule(ast) {
 				messageIdentifier = node.id.name;
 			}
 
+			// TODO: Support legacy non-class functions
 			if (
 				messageIdentifier !== null &&
 				node.type === Syntax.ClassDeclaration &&
 				node.superClass?.type === Syntax.Identifier &&
 				node.superClass.name === messageIdentifier
 			) {
-				TraverseClass(node.body);
+				messages.push(TraverseClass(node.body));
 				this.skip();
 			}
 		},
 	});
+
+	return messages;
 }
 
 function TraverseClass(ast) {
-	let className = null;
+	const message = {
+		className: null,
+		fields: [],
+	};
 
 	traverse(ast, {
 		enter: function (node) {
 			if (node.type === Syntax.MethodDefinition && node.key.type === Syntax.Identifier && node.key.name === "M") {
-				TraverseFields(node.value);
+				message.fields = TraverseFields(node.value);
 				this.skip();
 			}
 
@@ -68,29 +106,102 @@ function TraverseClass(ast) {
 				node.key.type === Syntax.Identifier &&
 				node.key.name === "getClassName"
 			) {
-				className = GetClassNameLiteral(node.value);
+				message.className = GetClassNameLiteral(node.value);
 				this.skip();
 			}
 		},
 	});
 
-	if (className === null) {
+	if (message.className === null) {
 		throw new Error("Failed to find classname");
 	}
+
+	return message;
 }
 
 function TraverseFields(ast) {
+	const fields = [];
+
 	traverse(ast, {
 		enter: function (node) {
 			if (node.type === Syntax.Property && node.key.type === Syntax.Identifier && node.key.name === "fields") {
 				for (const prop of node.value.properties) {
-					console.log(prop.key.name);
+					if (prop.type !== Syntax.Property || prop.value.type !== Syntax.ObjectExpression) {
+						throw new Error("Unexpected property");
+					}
+
+					const field = {
+						id: null,
+						name: prop.key.name,
+						type: null,
+						flag: "optional",
+					};
+
+					for (const fieldProp of prop.value.properties) {
+						if (fieldProp.key.name === "n") {
+							if (fieldProp.value.type === Syntax.Literal) {
+								field.id = fieldProp.value.value;
+							} else {
+								throw new Error("Unexpected field.n");
+							}
+						} else if (fieldProp.key.name === "r") {
+							if (EvaluateConstant(fieldProp)) {
+								field.flag = "repeated";
+							}
+						} else if (fieldProp.key.name === "d") {
+							field.default = EvaluateConstant(fieldProp);
+						} else if (fieldProp.key.name === "c") {
+							if (fieldProp.value.type === Syntax.Identifier) {
+								// TODO: Look it up
+								//field.typeToLookup = fieldProp.value.name;
+							} else if (fieldProp.value.type === Syntax.MemberExpression) {
+								// TODO: Looking up proto message from another module
+							} else {
+								throw new Error("Unexpected field.c");
+							}
+						} else if (fieldProp.key.name === "q") {
+							// What is "q"?
+						} else if (fieldProp.key.name === "br") {
+							if (
+								fieldProp.value.type === Syntax.MemberExpression &&
+								fieldProp.value.property.type === Syntax.Identifier
+							) {
+								field.type = fieldProp.value.property.name;
+
+								if (!field.type.startsWith("read")) {
+									throw new Error("Unexpected field.br");
+								}
+
+								field.type = field.type.substring(4).toLowerCase();
+
+								if (field.type.endsWith("64string")) {
+									field.type = field.type.substring(0, field.type.length - 6);
+								}
+
+								if (field.type === "enum") {
+									// TODO: RE enums
+									field.type = "int32";
+									field.description = "enum";
+								}
+							} else {
+								throw new Error("Unexpected field.br");
+							}
+						} else if (fieldProp.key.name === "bw") {
+							//
+						} else {
+							throw new Error("Unexpected field");
+						}
+					}
+
+					fields.push(field);
 				}
 
 				this.skip();
 			}
 		},
 	});
+
+	return fields;
 }
 
 function GetClassNameLiteral(ast) {
@@ -105,6 +216,16 @@ function GetClassNameLiteral(ast) {
 	});
 
 	return value;
+}
+
+function EvaluateConstant(node) {
+	if (node.value.type === Syntax.UnaryExpression && node.value.operator === "!") {
+		return node.value.argument.value === 0 ? true : false;
+	} else if (node.value.type === Syntax.Literal) {
+		return node.value.value;
+	}
+
+	throw new Error("Unexpected constant");
 }
 
 function GetListOfFilesToParse(dirName) {
