6397e42986498a45cd4adb775dc285c35d2d7e3c
Steam Database <freeman@steamdb.info>
2022-03-22 10:21:40 +0000

    Extract json files from -json.js files

--- ClientExtracted/steamui/39.js
+++
@@ -1,9 +0,0 @@
-(window.webpackJsonp = window.webpackJsonp || []).push([
-  [39],
-  {
-    jJmR: function (a) {
-      a.exports = JSON.parse('{"language":"arabic"}');
-    },
-  },
-]);
-//# sourceMappingURL=39.js.map
