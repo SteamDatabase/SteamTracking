70735b131d0f02ad3ea5e95f3b94aa3e67f26c1b
Steam Database <freeman@steamdb.info>
2022-03-26 01:07:01 +0000

    2 files | steampal, main.js

--- www.dota2.com/public/javascript/dota_react/main.js
+++ www.dota2.com/public/javascript/dota_react/main.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "7175958";
+var CLSTAMP = "7176157";
 (window.webpackJsonp = window.webpackJsonp || []).push([
   [2],
   {
