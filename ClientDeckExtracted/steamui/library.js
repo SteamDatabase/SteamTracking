bb23238185062a7b19bab8182643927ab852bcd7
Steam Database <freeman@steamdb.info>
2022-08-30 22:06:50 +0000

    5 files | 8501.js, libraries_cm.js, library.js, sp.js, steamdeck_publicbeta

--- ClientDeckExtracted/steamui/library.js
+++ ClientDeckExtracted/steamui/library.js
@@ -1,5 +1,5 @@
 /* Third-party software licenses can be found at licenses.txt */
-var CLSTAMP = "7468767";
+var CLSTAMP = "7472758";
 (() => {
   var e,
     t,
@@ -12575,9 +12575,9 @@ var CLSTAMP = "7468767";
                 ? !{
                     NODE_ENV: "production",
                     STEAM_BUILD: "buildbot",
-                    BUILD_TIME_LOCAL: "Aug 26 2022 : 18:24:42",
-                    BUILD_TIME_UTC: "Aug 27 2022 : 01:24:42",
-                    BUILD_RTIME_UTC: 1661563482,
+                    BUILD_TIME_LOCAL: "Aug 29 2022 : 18:07:05",
+                    BUILD_TIME_UTC: "Aug 30 2022 : 01:07:05",
+                    BUILD_RTIME_UTC: 1661821625,
                   }.MOBILE_BUILD && document.getElementById(t)
                 : t),
             n)
--- ClientDeckExtracted/steamui/sp.js
+++ ClientDeckExtracted/steamui/sp.js
@@ -10904,7 +10904,7 @@
           l = o ? Do(o) : "";
         if (!e) return null;
         const s = e.nSteamVersion > 0 ? e.nSteamVersion.toString() : "local",
-          c = parseInt(1661563482),
+          c = parseInt(1661821625),
           m = c && $o(c, t, l),
           d = e.nCPUHz / 1e3 / 1e3 / 1e3 + " GHz",
           p = (0, so.l)(1024 * e.nSystemRAMSizeMB * 1024),
@@ -25526,9 +25526,9 @@
             {
               NODE_ENV: "production",
               STEAM_BUILD: "buildbot",
-              BUILD_TIME_LOCAL: "Aug 26 2022 : 18:24:42",
-              BUILD_TIME_UTC: "Aug 27 2022 : 01:24:42",
-              BUILD_RTIME_UTC: 1661563482,
+              BUILD_TIME_LOCAL: "Aug 29 2022 : 18:07:05",
+              BUILD_TIME_UTC: "Aug 30 2022 : 01:07:05",
+              BUILD_RTIME_UTC: 1661821625,
             }.MOBILE_BUILD || window.addEventListener("unload", this.OnUnload);
         }
         OnUnload() {
