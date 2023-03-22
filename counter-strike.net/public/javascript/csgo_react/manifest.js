ae23a5ec2799ce39668b3fd79aad4356d2e95c0f
Steam Database <freeman@steamdb.info>
2023-03-22 16:52:19 +0000

    4 files | main.css, main.js, main_english.json, manifest.js

---
+++ counter-strike.net/public/javascript/csgo_react/manifest.js
@@ -0,0 +1,325 @@
+/**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
+ ****/
+var CLSTAMP = "7939085";
+(() => {
+  "use strict";
+  var e,
+    a,
+    n,
+    r,
+    s,
+    o = {},
+    t = {};
+  function c(e) {
+    var a = t[e];
+    if (void 0 !== a) return a.exports;
+    var n = (t[e] = { id: e, loaded: !1, exports: {} });
+    return o[e].call(n.exports, n, n.exports, c), (n.loaded = !0), n.exports;
+  }
+  (c.m = o),
+    (e = []),
+    (c.O = (a, n, r, s) => {
+      if (!n) {
+        var o = 1 / 0;
+        for (f = 0; f < e.length; f++) {
+          for (var [n, r, s] = e[f], t = !0, i = 0; i < n.length; i++)
+            (!1 & s || o >= s) && Object.keys(c.O).every((e) => c.O[e](n[i]))
+              ? n.splice(i--, 1)
+              : ((t = !1), s < o && (o = s));
+          if (t) {
+            e.splice(f--, 1);
+            var d = r();
+            void 0 !== d && (a = d);
+          }
+        }
+        return a;
+      }
+      s = s || 0;
+      for (var f = e.length; f > 0 && e[f - 1][2] > s; f--) e[f] = e[f - 1];
+      e[f] = [n, r, s];
+    }),
+    (c.n = (e) => {
+      var a = e && e.__esModule ? () => e.default : () => e;
+      return c.d(a, { a }), a;
+    }),
+    (n = Object.getPrototypeOf
+      ? (e) => Object.getPrototypeOf(e)
+      : (e) => e.__proto__),
+    (c.t = function (e, r) {
+      if ((1 & r && (e = this(e)), 8 & r)) return e;
+      if ("object" == typeof e && e) {
+        if (4 & r && e.__esModule) return e;
+        if (16 & r && "function" == typeof e.then) return e;
+      }
+      var s = Object.create(null);
+      c.r(s);
+      var o = {};
+      a = a || [null, n({}), n([]), n(n)];
+      for (var t = 2 & r && e; "object" == typeof t && !~a.indexOf(t); t = n(t))
+        Object.getOwnPropertyNames(t).forEach((a) => (o[a] = () => e[a]));
+      return (o.default = () => e), c.d(s, o), s;
+    }),
+    (c.d = (e, a) => {
+      for (var n in a)
+        c.o(a, n) &&
+          !c.o(e, n) &&
+          Object.defineProperty(e, n, { enumerable: !0, get: a[n] });
+    }),
+    (c.f = {}),
+    (c.e = (e) =>
+      Promise.all(Object.keys(c.f).reduce((a, n) => (c.f[n](e, a), a), []))),
+    (c.u = (e) =>
+      "javascript/csgo_react/" +
+      {
+        647: "main_czech-json",
+        698: "main_dutch-json",
+        727: "main_japanese-json",
+        799: "main_german-json",
+        1117: "main_french-json",
+        1162: "shared_thai-json",
+        1313: "shared_bulgarian-json",
+        2029: "shared_sc_schinese-json",
+        2431: "shared_arabic-json",
+        2443: "shared_latam-json",
+        2537: "shared_schinese-json",
+        2767: "main_polish-json",
+        2844: "shared_japanese-json",
+        2848: "main_ukrainian-json",
+        2942: "main_tchinese-json",
+        3112: "shared_french-json",
+        3185: "main_swedish-json",
+        3252: "shared_polish-json",
+        3277: "main_romanian-json",
+        3323: "main_danish-json",
+        3359: "main_bulgarian-json",
+        3557: "shared_finnish-json",
+        3768: "main_finnish-json",
+        4108: "shared_norwegian-json",
+        4189: "shared_czech-json",
+        4297: "shared_greek-json",
+        4722: "main_koreana-json",
+        4823: "shared_romanian-json",
+        4860: "main_russian-json",
+        4964: "shared_vietnamese-json",
+        5438: "shared_turkish-json",
+        5625: "shared_swedish-json",
+        5849: "shared_portuguese-json",
+        5925: "shared_dutch-json",
+        5933: "shared_italian-json",
+        5948: "shared_hungarian-json",
+        6007: "shared_german-json",
+        6087: "main_latam-json",
+        6492: "shared_spanish-json",
+        6542: "shared_koreana-json",
+        6815: "main_english-json",
+        6817: "main_spanish-json",
+        7236: "main_portuguese-json",
+        7602: "shared_tchinese-json",
+        7781: "shared_ukrainian-json",
+        7890: "main_thai-json",
+        7951: "main_norwegian-json",
+        7975: "shared_english-json",
+        8051: "main_hungarian-json",
+        8155: "main_italian-json",
+        8168: "main_vietnamese-json",
+        8385: "shared_brazilian-json",
+        9062: "shared_russian-json",
+        9431: "main_brazilian-json",
+        9568: "main_greek-json",
+        9663: "main_turkish-json",
+        9753: "shared_danish-json",
+        9980: "main_schinese-json",
+      }[e] +
+      ".js?contenthash=" +
+      {
+        647: "56e11d10d29caccb6b68",
+        698: "a682eb6072d75a11c1b2",
+        727: "613ecfafbc8c5413482a",
+        799: "72a56c355077900a349e",
+        1117: "bcf81dcd8ab94947c6ae",
+        1162: "6c34374a8bdd8a3e8f90",
+        1313: "786bf209d6ea794d54bd",
+        2029: "f8f0bd0a82da5000b5e5",
+        2431: "90f21d1a5a8046a0eb7e",
+        2443: "7895136a556ae79b0ca5",
+        2537: "278ef23343812eb6e6f7",
+        2767: "b4a1f379ac71a5bd1cc3",
+        2844: "a959b4fbabc86a2424b4",
+        2848: "d3e27c889ae14b8dcfd3",
+        2942: "a45066adec6bed2a03cc",
+        3112: "03b22c91d62cb42bf1f2",
+        3185: "22d9f719898420cdacc9",
+        3252: "4402c754b7c6652a2f79",
+        3277: "47f8441fd8180d26d9cd",
+        3323: "1ca42ab765d7f6d19bab",
+        3359: "bb00ede6c02e63ceb5c0",
+        3557: "b8d2539b51823016210c",
+        3768: "e891a90321753c1ee806",
+        4108: "9ff6b5d7ff361110be73",
+        4189: "413363a438df80eb9c57",
+        4297: "0052351ae0082f67b6d8",
+        4722: "7d47014a0158438a4211",
+        4823: "884aa9f7b069546ccd4b",
+        4860: "93e6419b02225e2e2855",
+        4964: "a0e42a8487b9bab980c1",
+        5438: "17428d252e66c4350413",
+        5625: "cc7971171f7fa69685e8",
+        5849: "fd1875b7356036c2ab51",
+        5925: "a52be718a27d69b8763c",
+        5933: "b08700009dc3f8d2d0b3",
+        5948: "53a0fd556033dae56687",
+        6007: "8bad76e735e467898598",
+        6087: "b007e010aafce466876b",
+        6492: "10cf54ecf934b0f2af0c",
+        6542: "659d5b50d7734017f56b",
+        6815: "8722d50574baac161a80",
+        6817: "f4650e394ade3f742c23",
+        7236: "992137fd95437361a23f",
+        7602: "05254b4430d003661fa9",
+        7781: "de36041ef5f405168905",
+        7890: "241493640fc13de1c182",
+        7951: "babcd0830127cb6948c0",
+        7975: "15b50baf874bd5308c62",
+        8051: "a73ac662f95a075a7895",
+        8155: "988cf2171f1c799d987c",
+        8168: "68fb22e9768885697b7e",
+        8385: "7a560ff5330a5c27a543",
+        9062: "a2fcd48f28ebd960c069",
+        9431: "776afdb24fb6b011c692",
+        9568: "45a7efedd512ca705f7b",
+        9663: "92e2be8ad31105de3362",
+        9753: "bfb24c6be80eff14df61",
+        9980: "6b6ae73c724d85c1ef1a",
+      }[e]),
+    (c.miniCssF = (e) => {}),
+    (c.g = (function () {
+      if ("object" == typeof globalThis) return globalThis;
+      try {
+        return this || new Function("return this")();
+      } catch (e) {
+        if ("object" == typeof window) return window;
+      }
+    })()),
+    (c.o = (e, a) => Object.prototype.hasOwnProperty.call(e, a)),
+    (r = {}),
+    (s = "csgo_react:"),
+    (c.l = (e, a, n, o) => {
+      if (r[e]) r[e].push(a);
+      else {
+        var t, i;
+        if (void 0 !== n)
+          for (
+            var d = document.getElementsByTagName("script"), f = 0;
+            f < d.length;
+            f++
+          ) {
+            var b = d[f];
+            if (
+              b.getAttribute("src") == e ||
+              b.getAttribute("data-webpack") == s + n
+            ) {
+              t = b;
+              break;
+            }
+          }
+        t ||
+          ((i = !0),
+          ((t = document.createElement("script")).charset = "utf-8"),
+          (t.timeout = 120),
+          c.nc && t.setAttribute("nonce", c.nc),
+          t.setAttribute("data-webpack", s + n),
+          (t.src = e)),
+          (r[e] = [a]);
+        var h = (a, n) => {
+            (t.onerror = t.onload = null), clearTimeout(l);
+            var s = r[e];
+            if (
+              (delete r[e],
+              t.parentNode && t.parentNode.removeChild(t),
+              s && s.forEach((e) => e(n)),
+              a)
+            )
+              return a(n);
+          },
+          l = setTimeout(
+            h.bind(null, void 0, { type: "timeout", target: t }),
+            12e4
+          );
+        (t.onerror = h.bind(null, t.onerror)),
+          (t.onload = h.bind(null, t.onload)),
+          i && document.head.appendChild(t);
+      }
+    }),
+    (c.r = (e) => {
+      "undefined" != typeof Symbol &&
+        Symbol.toStringTag &&
+        Object.defineProperty(e, Symbol.toStringTag, { value: "Module" }),
+        Object.defineProperty(e, "__esModule", { value: !0 });
+    }),
+    (c.nmd = (e) => ((e.paths = []), e.children || (e.children = []), e)),
+    (() => {
+      var e;
+      c.g.importScripts && (e = c.g.location + "");
+      var a = c.g.document;
+      if (!e && a && (a.currentScript && (e = a.currentScript.src), !e)) {
+        var n = a.getElementsByTagName("script");
+        n.length && (e = n[n.length - 1].src);
+      }
+      if (!e)
+        throw new Error(
+          "Automatic publicPath is not supported in this browser"
+        );
+      (e = e
+        .replace(/#.*$/, "")
+        .replace(/\?.*$/, "")
+        .replace(/\/[^\/]+$/, "/")),
+        (c.p = e + "../../");
+    })(),
+    (() => {
+      var e = { 6700: 0 };
+      (c.f.j = (a, n) => {
+        var r = c.o(e, a) ? e[a] : void 0;
+        if (0 !== r)
+          if (r) n.push(r[2]);
+          else if (6700 != a) {
+            var s = new Promise((n, s) => (r = e[a] = [n, s]));
+            n.push((r[2] = s));
+            var o = c.p + c.u(a),
+              t = new Error();
+            c.l(
+              o,
+              (n) => {
+                if (c.o(e, a) && (0 !== (r = e[a]) && (e[a] = void 0), r)) {
+                  var s = n && ("load" === n.type ? "missing" : n.type),
+                    o = n && n.target && n.target.src;
+                  (t.message =
+                    "Loading chunk " + a + " failed.\n(" + s + ": " + o + ")"),
+                    (t.name = "ChunkLoadError"),
+                    (t.type = s),
+                    (t.request = o),
+                    r[1](t);
+                }
+              },
+              "chunk-" + a,
+              a
+            );
+          } else e[a] = 0;
+      }),
+        (c.O.j = (a) => 0 === e[a]);
+      var a = (a, n) => {
+          var r,
+            s,
+            [o, t, i] = n,
+            d = 0;
+          if (o.some((a) => 0 !== e[a])) {
+            for (r in t) c.o(t, r) && (c.m[r] = t[r]);
+            if (i) var f = i(c);
+          }
+          for (a && a(n); d < o.length; d++)
+            (s = o[d]), c.o(e, s) && e[s] && e[s][0](), (e[s] = 0);
+          return c.O(f);
+        },
+        n = (self.webpackChunkcsgo_react = self.webpackChunkcsgo_react || []);
+      n.forEach(a.bind(null, 0)), (n.push = a.bind(null, n.push.bind(n)));
+    })();
+})();
