b27e90aaf3028c1085313a704587bf54cd17bc03
Steam Database <freeman@steamdb.info>
2024-02-21 00:04:14 +0000

    3 files | ticketmaster.js, styles_admin.css, spring_year_sale.css

--- help.steampowered.com/public/javascript/ticketmaster.js
+++ help.steampowered.com/public/javascript/ticketmaster.js
@@ -266,7 +266,7 @@ function SubmitReplyForm_Internal( form, extraFormData )
 	if ( extraFormData )
 	{
 		var vars = extraFormData.split('&');
-		for (var i = 0; i < vars.length; i++) 
+		for (var i = 0; i < vars.length; i++)
 		{
 			var pair = vars[i].split( '=' );
 			fd.append( decodeURIComponent( pair[0] ), decodeURIComponent( pair[1] ) );
