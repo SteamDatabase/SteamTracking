fdb5529e7c4728745bf5a9e2b425bf7c595f90e5
Steam Database <freeman@steamdb.info>
2024-12-02 20:05:04 +0000

    2 files | SteamMobileApks.txt, manifest.js

--- store.steampowered.com/public/javascript/applications/store/manifest.js
+++ store.steampowered.com/public/javascript/applications/store/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "9381792";
+var CLSTAMP = "9382313";
 (() => {
   "use strict";
   var e,
