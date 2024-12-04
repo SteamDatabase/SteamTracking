c627fe60adf41cf47518dcd1ac37f97ae015504e
Steam Database <freeman@steamdb.info>
2024-12-04 00:06:13 +0000

    24 files | ClientContentLinux.txt, chunk~2dcc5aaf7.js, libraries~00299a408.js, library.js, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steamchina_publicbeta_osx, steamchina_publicbeta_win32, steamdeck_publicbeta, steammessages_remoteplay.proto, crashhandler.txt, friendsui.txt, gameoverlayui.txt, libtier0_s.txt, libvideo.txt, steam.txt, steamclient.txt, steamservice.txt, steamui.txt, manifest.js

--- store.steampowered.com/public/javascript/applications/store/manifest.js
+++ store.steampowered.com/public/javascript/applications/store/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "9386344";
+var CLSTAMP = "9386790";
 (() => {
   "use strict";
   var e,
