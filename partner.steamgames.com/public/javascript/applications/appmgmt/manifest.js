0e033c2cdb4659ede5008a4c779df17cfc4b37f5
Steam Database <freeman@steamdb.info>
2025-01-08 20:04:59 +0000

    1 files | manifest.js

--- partner.steamgames.com/public/javascript/applications/appmgmt/manifest.js
+++ partner.steamgames.com/public/javascript/applications/appmgmt/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "9444214";
+var CLSTAMP = "9445657";
 (() => {
   "use strict";
   var e,
