99c3761a974b4920be04345efbd2a32ccef5ac32
Pavel Djundik <xPaw@users.noreply.github.com>
2025-05-07 15:42:31 +0300

    Update update.sh

--- update.sh
+++ update.sh
@@ -9,12 +9,28 @@ fi
 
 echo $$ > $LOCKFILE
 
+git_commit_message() {
+	local all_changed_files
+	all_changed_files=$(git status --porcelain)
+
+	local num_files
+	num_files=$(echo "$all_changed_files" | wc -l)
+
+	local non_c_files
+	non_c_files=$(echo "$all_changed_files" | grep -v "^.. c/")
+
+	local file_list
+	file_list=$(echo "$non_c_files" | awk '{print $2}' | xargs -n1 basename | sed '{:q;N;s/\n/, /g;t q}')
+
+	echo "$num_files files${file_list:+ | $file_list}"
+}
+
 #mkdir -p .support/{archives,bins,linux_archives,linux_bins,steampal_archives}
 
 php update.php "$1"
 
 git add -A
-git commit -S -a -m "$(git status --porcelain | wc -l) files | $(git status --porcelain|awk '{print "basename " $2}'| sh | sed '{:q;N;s/\n/, /g;t q}')"
+git commit -S -a -m "$(git_commit_message)"
 git push
 
 rm $LOCKFILE
