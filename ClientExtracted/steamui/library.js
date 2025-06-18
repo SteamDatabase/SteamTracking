7ac142b38550b0eeb71b00db9c87477ae670bcd4
Steam Database <freeman@steamdb.info>
2025-06-18 02:06:31 +0000

    36 files | JavascriptUrls.txt, ClientContentLinux.txt, ingamecpuinfo.layout, ingamefpsbanner.layout, ingamegamefps.layout, ingamegpuinfo.layout, ingamepresentationfps.layout, ingameraminfo.layout, gameoverlay.styles, chunk~2dcc5aaf7.js, chunk~2dcc5aaf7.css, libraries~00299a408.js, library.js, steamui_english.json, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steamchina_publicbeta_osx, steamchina_publicbeta_win32, steamdeck_publicbeta, steammessages_clientsettings.proto, steammessages_remoteplay.proto, crashhandler.txt, friendsui.txt, gameoverlayui.txt, libtier0_s.txt, libvideo.txt, steam.txt, steamclient.txt, steamservice.txt, steamui.txt

--- ClientExtracted/steamui/library.js
+++ ClientExtracted/steamui/library.js
@@ -1,4 +1,4 @@
-var CLSTAMP = "9834728";
+var CLSTAMP = "9839551";
 /* Third-party software licenses can be found at licenses.txt */ (() => {
   var e,
     t,
@@ -11142,9 +11142,9 @@ var CLSTAMP = "9834728";
               !{
                 NODE_ENV: "production",
                 STEAM_BUILD: "buildbot",
-                BUILD_TIME_LOCAL: "Jun 16 2025 : 14:29:36",
-                BUILD_TIME_UTC: "Jun 16 2025 : 21:29:36",
-                BUILD_RTIME_UTC: 1750109376,
+                BUILD_TIME_LOCAL: "Jun 17 2025 : 17:38:21",
+                BUILD_TIME_UTC: "Jun 18 2025 : 00:38:21",
+                BUILD_RTIME_UTC: 1750207101,
               }.MOBILE_BUILD &&
               "addEventListener" in window
             ) {
@@ -24655,9 +24655,9 @@ var CLSTAMP = "9834728";
                 ? {
                     NODE_ENV: "production",
                     STEAM_BUILD: "buildbot",
-                    BUILD_TIME_LOCAL: "Jun 16 2025 : 14:29:36",
-                    BUILD_TIME_UTC: "Jun 16 2025 : 21:29:36",
-                    BUILD_RTIME_UTC: 1750109376,
+                    BUILD_TIME_LOCAL: "Jun 17 2025 : 17:38:21",
+                    BUILD_TIME_UTC: "Jun 18 2025 : 00:38:21",
+                    BUILD_RTIME_UTC: 1750207101,
                   }.MOBILE_BUILD
                   ? null
                   : document.getElementById(t)
@@ -25245,7 +25245,7 @@ var CLSTAMP = "9834728";
         3180: "8e8aa27ac0cac69a5efc",
         3350: "25be1c0cde58ecd53fec",
         3366: "5854ad132884cbc8d12f",
-        3569: "53b872908151913b2814",
+        3569: "6dc9776af73e5cd43f9a",
         3583: "f831ab7edbd9ffa591ac",
         3594: "6773526db05f74dd2672",
         3714: "ab7010eec823473536d8",
@@ -25323,7 +25323,7 @@ var CLSTAMP = "9834728";
         9418: "29e9fa15a9fc8e62a893",
         9462: "77a210af3f848de941d1",
         9711: "2ed989ee7251a5d19c6f",
-        9858: "c0c16b6b564d92fb7c71",
+        9858: "7a1eb865cb2bf0a516b1",
         9869: "e0e020c0bad905ce16af",
         9887: "e51f90d2f92bc26f642e",
       }[e]),
