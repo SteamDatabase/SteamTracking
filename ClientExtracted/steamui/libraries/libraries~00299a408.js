2e7d2f46adf5b6c350df36bc1cb994ebf529a3ad
Steam Database <freeman@steamdb.info>
2025-06-30 22:06:26 +0000

    57 files | JavascriptUrls.txt, ClientContentLinux.txt, overlay_russian.txt, chunk~2dcc5aaf7.js, libraries~00299a408.js, library.js, shared_brazilian.json, shared_bulgarian.json, shared_czech.json, shared_danish.json, shared_dutch.json, shared_finnish.json, shared_french.json, shared_german.json, shared_greek.json, shared_hungarian.json, shared_indonesian.json, shared_italian.json, shared_japanese.json, shared_koreana.json, shared_latam.json, shared_norwegian.json, shared_polish.json, shared_portuguese.json, shared_romanian.json, shared_russian.json, shared_schinese.json, shared_spanish.json, shared_swedish.json, shared_tchinese.json, shared_thai.json, shared_turkish.json, shared_ukrainian.json, shared_vietnamese.json, steamui_dutch.json, steamui_finnish.json, steamui_french.json, steamui_german.json, steamui_romanian.json, steamui_ukrainian.json, steamui_vietnamese.json, steam_client_osx, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_client_ubuntu12, steam_client_win32, steam_cmd_linux, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steam_cmd_win32, steamchina_win32, steamdeck_publicbeta, steamdeck_stable

--- ClientExtracted/steamui/libraries/libraries~00299a408.js
+++ ClientExtracted/steamui/libraries/libraries~00299a408.js
@@ -1,4 +1,4 @@
-var CLSTAMP = "9866038";
+var CLSTAMP = "9867751";
 /* Third-party software licenses can be found at licenses.txt */ (self.webpackChunksteamui =
   self.webpackChunksteamui || []).push([
   [41],
