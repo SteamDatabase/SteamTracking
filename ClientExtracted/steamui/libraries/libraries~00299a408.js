7493d390f4427b49c9d3e2ab906d22c362ff5727
Steam Database <freeman@steamdb.info>
2025-07-01 22:05:57 +0000

    77 files | ClientContentLinux.txt, chunk~2dcc5aaf7.js, libraries~00299a408.js, library.js, shared_czech.json, shared_danish.json, shared_dutch.json, shared_english.json, shared_finnish.json, shared_french.json, shared_german.json, shared_greek.json, shared_hungarian.json, shared_indonesian.json, shared_italian.json, shared_japanese.json, shared_koreana.json, shared_latam.json, shared_norwegian.json, shared_romanian.json, shared_russian.json, shared_schinese.json, shared_spanish.json, shared_swedish.json, shared_tchinese.json, shared_thai.json, shared_turkish.json, shared_ukrainian.json, shared_vietnamese.json, steamui_brazilian.json, steamui_bulgarian.json, steamui_czech.json, steamui_danish.json, steamui_dutch.json, steamui_english.json, steamui_finnish.json, steamui_french.json, steamui_german.json, steamui_greek.json, steamui_hungarian.json, steamui_indonesian.json, steamui_italian.json, steamui_japanese.json, steamui_koreana.json, steamui_latam.json, steamui_norwegian.json, steamui_polish.json, steamui_portuguese.json, steamui_romanian.json, steamui_russian.json, steamui_schinese.json, steamui_spanish.json, steamui_swedish.json, steamui_tchinese.json, steamui_thai.json, steamui_turkish.json, steamui_ukrainian.json, steamui_vietnamese.json, steam_client_osx, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_client_ubuntu12, steam_client_win32, steam_cmd_linux, steam_cmd_osx, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steam_cmd_win32, steamchina_publicbeta_osx, steamchina_publicbeta_win32, steamchina_win32, steamdeck_publicbeta, steamdeck_stable

--- ClientExtracted/steamui/libraries/libraries~00299a408.js
+++ ClientExtracted/steamui/libraries/libraries~00299a408.js
@@ -1,4 +1,4 @@
-var CLSTAMP = "9870011";
+var CLSTAMP = "9871360";
 /* Third-party software licenses can be found at licenses.txt */ (self.webpackChunksteamui =
   self.webpackChunksteamui || []).push([
   [41],
