74b13b7d7b08c606bf5059be8096d68ba5aeea39
Steam Database <freeman@steamdb.info>
2025-07-23 00:05:07 +0000

    4 files | game.css, main.js, manifest.js

--- www.dota2.com/public/javascript/dota_react/manifest.js
+++ www.dota2.com/public/javascript/dota_react/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "9918473";
+var CLSTAMP = "9919424";
 (() => {
   "use strict";
   var e,
