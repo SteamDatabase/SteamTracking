0b6d3dc23db0a20a99611f23385be1774bdde326
Steam Database <freeman@steamdb.info>
2025-08-18 18:04:12 +0000

    2 files | extract_client.sh, update.sh

--- extract_client.sh
+++ extract_client.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+set -euo pipefail
 
 #
 # PREPARE EVERYTHING
@@ -6,18 +7,19 @@
 
 export LC_ALL=C
 
-DIR="$(dirname "$(readlink -f "$0")")"
+SCRIPT_LOCATION="$(readlink -f "$0")"
+DIR="$(dirname "${SCRIPT_LOCATION}")"
 
-cd "$DIR/.support" || exit 1
+cd "${DIR}/.support" || exit 1
 
 echo Deleting existing files
 
 # Scary!
 rm -rf linux_bins/*
-rm -rf "$DIR"/ClientExtracted/*
-rm -rf "$DIR"/c/ClientExtracted/*
-rm -f "$DIR"/BuildbotPaths/*
-rm -rf "$DIR"/Protobufs/*
+rm -rf "${DIR}"/ClientExtracted/*
+rm -rf "${DIR}"/c/ClientExtracted/*
+rm -f "${DIR}"/BuildbotPaths/*
+rm -rf "${DIR}"/Protobufs/*
 
 #
 # EXTRACT EVERYTHING
@@ -27,17 +29,17 @@ echo Extracting archives
 
 for z in linux_archives/*.zip;
 do
-	if [[ "$z" == *"bins"* ]] || [[ "$z" == *"runtime_"* ]];
+	if [[ "${z}" == *"bins"* ]] || [[ "${z}" == *"runtime_"* ]];
 	then
-		unzip -q -n "$z" -d linux_bins/
+		unzip -q -n "${z}" -d linux_bins/
 	else
-		unzip -q -o "$z" -d "$DIR/ClientExtracted/"
+		unzip -q -o "${z}" -d "${DIR}/ClientExtracted/"
 	fi
 
-	unzip -lv "$z" | head -n -2 | tail -n+4 | awk '{print $(NF-1),$NF}' >> "$DIR/ClientExtracted/ClientContentLinux.txt"
+	unzip -lv "${z}" | head -n -2 | tail -n+4 | awk '{print $(NF-1),$NF}' >> "${DIR}/ClientExtracted/ClientContentLinux.txt"
 done
 
-sort -k 2 -o "$DIR/ClientExtracted/ClientContentLinux.txt" "$DIR/ClientExtracted/ClientContentLinux.txt"
+sort -k 2 -o "${DIR}/ClientExtracted/ClientContentLinux.txt" "${DIR}/ClientExtracted/ClientContentLinux.txt"
 
 #
 # PROTOBUF DUMP
@@ -45,10 +47,10 @@ sort -k 2 -o "$DIR/ClientExtracted/ClientContentLinux.txt" "$DIR/ClientExtracted
 
 echo Dumping protobufs
 
-PROTOBUF_DUMPER="$DIR/SteamKit/Resources/ProtobufDumper/ProtobufDumper/bin/Release/linux-x64/publish/ProtobufDumper"
+PROTOBUF_DUMPER="${DIR}/SteamKit/Resources/ProtobufDumper/ProtobufDumper/bin/Release/linux-x64/publish/ProtobufDumper"
 
-"$PROTOBUF_DUMPER" linux_bins/ubuntu12_32/steamui.so "$DIR/Protobufs/" > /dev/null
-"$PROTOBUF_DUMPER" linux_bins/ubuntu12_32/steamclient.so "$DIR/Protobufs/" > /dev/null
+"${PROTOBUF_DUMPER}" linux_bins/ubuntu12_32/steamui.so "${DIR}/Protobufs/" > /dev/null
+"${PROTOBUF_DUMPER}" linux_bins/ubuntu12_32/steamclient.so "${DIR}/Protobufs/" > /dev/null
 
 # https://github.com/m4dEngi/steamworks_dumper
 echo Dumping structs
@@ -63,14 +65,14 @@ echo Dumping strings
 
 while IFS= read -r -d '' file
 do
-	echo "Dumping $file"
+	echo "Dumping ${file}"
 
-	name=$(basename "$file" .so)
+	name=$(basename "${file}" .so)
 
-	"$DIR/DumpStrings/DumpStrings" -target elf -binary "$file" | sort --unique > "$DIR/Strings/$name.txt"
+	"${DIR}/DumpStrings/DumpStrings" -target elf -binary "${file}" | sort --unique > "${DIR}/Strings/${name}.txt"
 done <   <(find linux_bins/ubuntu12_32/ -name '*.so' -print0)
 
-"$DIR/DumpStrings/DumpStrings" -target elf -binary "$DIR/ClientExtracted/ubuntu12_32/steam" | sort --unique > "$DIR/Strings/steam.txt"
+"${DIR}/DumpStrings/DumpStrings" -target elf -binary "${DIR}/ClientExtracted/ubuntu12_32/steam" | sort --unique > "${DIR}/Strings/steam.txt"
 
 #
 # Jump to extracted folder
@@ -86,31 +88,35 @@ ProcessClientFolder()
 
 	while IFS= read -r -d '' file
 	do
-		fullpath="$DIR/ClientExtracted/$file"
+		fullpath="${DIR}/ClientExtracted/${file}"
 
-		if [[ "$file" == *.js ]]
+		if [[ "${file}" == *.js ]]
 		then
-			php "$DIR/extract_json_from_webpack.php" "$fullpath"
-			if [[ $? -eq 200 ]]
+			set +e
+			php "${DIR}/extract_json_from_webpack.php" "${fullpath}"
+			EXTRACT_CODE="$?"
+			set -e
+			if [[ "${EXTRACT_CODE}" -eq 200 ]]
 			then
-				echo "Extracted json from $file"
+				echo "Extracted json from ${file}"
 				continue
 			fi
 
-			filename=$(basename "$file")
-			if [[ "$filename" != "manifest.js" && "$filename" != "licenses.js" ]]
+			filename=$(basename "${file}")
+			if [[ "${filename}" != "manifest.js" && "${filename}" != "licenses.js" ]]
 			then
-				cleandir="$DIR/c/ClientExtracted/$(dirname "$file")"
-				cleanfile="$DIR/c/ClientExtracted/$file"
-				mkdir -p "$cleandir"
+				cleandir="${DIR}/c/ClientExtracted/$(dirname "${file}")"
+				cleanfile="${DIR}/c/ClientExtracted/${file}"
+				mkdir -p "${cleandir}"
 
-				node "$DIR/generate_clean_js.mjs" "$fullpath" "$cleanfile" && npm run prettier "$cleanfile"
+				node "${DIR}/generate_clean_js.mjs" "${fullpath}" "${cleanfile}"
+				npm run prettier "${cleanfile}" || true
 			fi
 		fi
 
-		echo "Prettifying $file"
+		echo "Prettifying ${file}"
 
-		npm run prettier "$fullpath"
+		npm run prettier "${fullpath}" || true
 	done <   <(find steamui/ clientui/ siteserverui/ \( -name '*.js' -o -name '*.css' \) -print0)
 
 	#
@@ -121,14 +127,16 @@ ProcessClientFolder()
 
 	while IFS= read -r -d '' file
 	do
-		encoding=$(file -bi "$file" | sed -e 's/.*[ ]charset=//');
+		encoding=$(file -bi "${file}" | sed -e 's/.*[ ]charset=//');
 
-		if [[ "$encoding" != "utf-8" ]] && [[ "$encoding" != "binary" ]];
+		if [[ "${encoding}" != "utf-8" ]] && [[ "${encoding}" != "binary" ]];
 		then
-			iconv -f "$encoding" -t UTF-8 "$file" -o "$file.tmp" && mv "$file.tmp" "$file"
+			iconv -f "${encoding}" -t UTF-8 "${file}" -o "${file}.tmp" && mv "${file}.tmp" "${file}"
 		fi
 	done <   <(find . \( -name '*.txt' -o -name '*.xml' -o -name '*.cfg' -o -name '*.res' \) -print0)
 }
 
-cd "$DIR/ClientExtracted/" || exit 1
+cd "${DIR}/ClientExtracted/" || exit 1
 ProcessClientFolder
+
+echo "Done extracting client"
--- update.sh
+++ update.sh
@@ -1,36 +1,36 @@
 #!/bin/bash
+set -euo pipefail
 
 LOCKFILE=.support/update.lock
 
-if [[ -f "$LOCKFILE" ]] && kill -0 "$(cat "$LOCKFILE")" 2>/dev/null; then
+if [[ -f "${LOCKFILE}" ]] && kill -0 "$(cat "${LOCKFILE}" || true)" 2>/dev/null; then
 	echo Still running
 	exit 1
 fi
 
-echo $$ > "$LOCKFILE"
+echo $$ > "${LOCKFILE}"
 
 git_commit_message() {
 	local all_changed_files
 	all_changed_files=$(git status --porcelain)
 
 	local num_files
-	num_files=$(echo "$all_changed_files" | wc -l)
+	num_files=$(echo "${all_changed_files}" | wc -l)
 
 	local non_c_files
-	non_c_files=$(echo "$all_changed_files" | grep -v "^.. c/")
+	non_c_files=$(echo "${all_changed_files}" | grep -v "^.. c/")
 
 	local file_list
-	file_list=$(echo "$non_c_files" | awk '{print $2}' | xargs -n1 basename | sed '{:q;N;s/\n/, /g;t q}')
+	file_list=$(echo "${non_c_files}" | awk '{print $2}' | xargs -r -n1 basename | sed '{:q;N;s/\n/, /g;t q}')
 
-	echo "$num_files files${file_list:+ | $file_list}"
+	echo "${num_files} files${file_list:+ | ${file_list}}"
 }
 
-#mkdir -p .support/{archives,bins,linux_archives,linux_bins,steampal_archives}
-
-php update.php "$1"
+php update.php "$@"
 
 git add -A
-git commit -S -a -m "$(git_commit_message)"
+MESSAGE=$(git_commit_message)
+git commit -S -a -m "${MESSAGE}"
 git push
 
-rm "$LOCKFILE"
+rm "${LOCKFILE}"
