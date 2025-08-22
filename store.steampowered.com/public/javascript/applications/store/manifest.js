ae8a9ed8f16b923a00705acf4c9ef2c11d1b9fa9
Steam Database <freeman@steamdb.info>
2025-08-22 22:05:40 +0000

    95 files | main.css, game.css, manifest.js

--- store.steampowered.com/public/javascript/applications/store/manifest.js
+++ store.steampowered.com/public/javascript/applications/store/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "10006262";
+var CLSTAMP = "10009604";
 (() => {
   "use strict";
   var e,
