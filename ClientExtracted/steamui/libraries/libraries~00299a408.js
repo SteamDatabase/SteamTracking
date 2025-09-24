766162424423c5c299047a4b079173eaeccbb5d4
Steam Database <freeman@steamdb.info>
2025-09-24 01:06:15 +0000

    25 files | ClientContentLinux.txt, chunk~2dcc5aaf7.js, chunk~2dcc5aaf7.css, libraries~00299a408.js, library.js, steamui_english.json, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steamchina_publicbeta_osx, steamchina_publicbeta_win32, steamdeck_publicbeta, crashhandler.txt, gameoverlayui.txt, libtier0_s.txt, libvideo.txt, steam.txt, steamclient.txt, steamservice.txt, steamui.txt

--- ClientExtracted/steamui/libraries/libraries~00299a408.js
+++ ClientExtracted/steamui/libraries/libraries~00299a408.js
@@ -1,4 +1,4 @@
-var CLSTAMP = "10089048";
+var CLSTAMP = "10092052";
 /* Third-party software licenses can be found at licenses.txt */ (self.webpackChunksteamui =
   self.webpackChunksteamui || []).push([
   [41],
