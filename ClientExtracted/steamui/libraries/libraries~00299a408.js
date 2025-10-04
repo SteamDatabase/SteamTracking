fd0954f0e320c6577a27910237efb6af054d0f8c
Steam Database <freeman@steamdb.info>
2025-10-04 00:06:17 +0000

    86 files | ClientContentLinux.txt, chunk~2dcc5aaf7.js, libraries~00299a408.js, library.js, friendsui_czech.json, friendsui_finnish.json, friendsui_koreana.json, friendsui_sc_schinese.json, friendsui_spanish.json, shared_brazilian.json, shared_bulgarian.json, shared_czech.json, shared_danish.json, shared_dutch.json, shared_finnish.json, shared_french.json, shared_german.json, shared_greek.json, shared_hungarian.json, shared_indonesian.json, shared_italian.json, shared_japanese.json, shared_koreana.json, shared_latam.json, shared_norwegian.json, shared_polish.json, shared_portuguese.json, shared_romanian.json, shared_russian.json, shared_sc_schinese.json, shared_schinese.json, shared_spanish.json, shared_swedish.json, shared_tchinese.json, shared_thai.json, shared_turkish.json, shared_ukrainian.json, shared_vietnamese.json, steamui_brazilian.json, steamui_bulgarian.json, steamui_czech.json, steamui_danish.json, steamui_dutch.json, steamui_finnish.json, steamui_french.json, steamui_german.json, steamui_greek.json, steamui_hungarian.json, steamui_indonesian.json, steamui_italian.json, steamui_japanese.json, steamui_koreana.json, steamui_latam.json, steamui_norwegian.json, steamui_polish.json, steamui_portuguese.json, steamui_romanian.json, steamui_russian.json, steamui_sc_schinese.json, steamui_schinese.json, steamui_spanish.json, steamui_swedish.json, steamui_tchinese.json, steamui_thai.json, steamui_turkish.json, steamui_ukrainian.json, steamui_vietnamese.json, steam_client_publicbeta_osx, steam_client_publicbeta_ubuntu12, steam_client_publicbeta_win32, steam_cmd_publicbeta_linux, steam_cmd_publicbeta_osx, steam_cmd_publicbeta_win32, steamchina_publicbeta_osx, steamchina_publicbeta_win32, steamdeck_publicbeta, crashhandler.txt, gameoverlayui.txt, libtier0_s.txt, libvideo.txt, steam.txt, steamclient.txt, steamservice.txt, steamui.txt

--- ClientExtracted/steamui/libraries/libraries~00299a408.js
+++ ClientExtracted/steamui/libraries/libraries~00299a408.js
@@ -1,4 +1,4 @@
-var CLSTAMP = "10117681";
+var CLSTAMP = "10122598";
 /* Third-party software licenses can be found at licenses.txt */ (self.webpackChunksteamui =
   self.webpackChunksteamui || []).push([
   [41],
