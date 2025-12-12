54ab50d174ea9ece0290a10967429cc070263bef
Steam Database <freeman@steamdb.info>
2025-12-12 21:05:29 +0000

    2 files | manifest.js, game_mob.css

--- partner.steamgames.com/public/javascript/applications/appmgmt/manifest.js
+++ partner.steamgames.com/public/javascript/applications/appmgmt/manifest.js
@@ -1,6 +1,6 @@
 /**** (c) Valve Corporation. Use is governed by the terms of the Steam Subscriber Agreement http://store.steampowered.com/subscriber_agreement/.
  ****/
-var CLSTAMP = "10309436";
+var CLSTAMP = "10309489";
 (() => {
   "use strict";
   var e,
